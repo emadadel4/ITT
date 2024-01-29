6527f4cec368f4d61edf91dbe447306a7c55a9c4
Emad Adel <emadadel1920@gmail.com>
2024-01-29 23:43:38 +0200

    ITT

--- itt.ps1
+++ itt.ps1
@@ -19,14 +19,13 @@ Write-Output "||_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____||";
     Write-Host "1: Microsoft Activation "
     Write-Host "2: Chris Titus Tech's Windows Utility"
     Write-Host "3: Fix Stutter in-Games [Windows 10/11]"
-    Write-Host "4: Fix Stutter in-Games [Windows 10/11]"
+    Write-Host "4: Download and Install VLC"
 
 
 
     Write-Host "5: Emad Adel [Github]"
     Write-Host "6: Emad Adel [Telgram]"
     Write-Host "Q: Press 'Q' to quit"
-
    
 }
 
@@ -34,47 +33,36 @@ do {
     Show-Menu -Title 'Emad Utility'
     $selection = Read-Host "Please make a selection"
     switch ($selection) {
-        '1' {irm https://massgrave.dev/get | Invoke-Expression}
+        '1'
+        {
+            Invoke-RestMethod https://massgrave.dev/get | Invoke-Expression
+        }
         
-        '2' {Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression}
+        '2'
+        {
+            Invoke-WebRequest -useb https://christitus.com/win | Invoke-Expression
+        }
 
-        '3' {
+        '3'
+        {
             Invoke-WebRequest "https://raw.githubusercontent.com/emadadel4/Fix-Stutter-in-Games/main/Fix%20Stutter%20Games%20%5Brun%20as%20administrator%5D.bat" -outfile "fix.bat"
             Start-Process -FilePath "fix.bat"
         }
 
         '4'
         {
-            $FileUri = "https://get.videolan.org/vlc/3.0.20/win64/vlc-3.0.20-win64.exe"
-            $Destination = "vlc-3.0.20-win64.exe"
-
-            $bitsJobObj = Start-BitsTransfer $FileUri -Destination $Destination
-
-            switch ($bitsJobObj.JobState) {
-
-            'Transferred' {
-             Complete-BitsTransfer -BitsJob $bitsJobObj
-             break
-            }
-
-        'Error' {
-          throw 'Error downloading'
-        }
-}
-
-$exeArgs = '/verysilent /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
-
-Start-Process -Wait $Destination -ArgumentList $exeArgs
+            
         }
 
+        '5'  
+        {
+            Start-Process "https://www.github.com/emadadel4"
+        }
 
-
-        '4'  {Start-Process "https://www.github.com/emadadel4"}
-
-        '6'  {Start-Process "https://t.me/emadadel4"
-
-    
-    }
+        '6'
+        {
+            Start-Process "https://t.me/emadadel4"
+        }
 
         'Q' {
             Write-Host "Exiting menu..."
