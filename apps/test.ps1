418ad7e0f21ff8dfc40e1ad52b8001e792b57394
Emad Adel <emadadel1920@gmail.com>
2024-02-01 00:53:20 +0200

    ITT

--- apps/test.ps1
+++ apps/test.ps1
@@ -1,6 +1,6 @@
-$url = "https://cdn.stubdownloader.services.mozilla.com/builds/firefox-stub/en-US/win/69460ea617d4d1460a08bcaac90553132695df037332248fcc389ac5ca0dfebf/Firefox Installer.exe"
-$outpath = "$PSScriptRoot/myexe.exe"
+$url = "https://update.code.visualstudio.com/latest/win32-x64-user/stable"
+$outpath = "$env:temp/myexe.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
 
-$args = @("Comma","Separated","Arguments")
-Start-Process -Filepath "$PSScriptRoot/myexe.exe" -ArgumentList $args
+$args = '/VERYSILENT /S /Q /quiet /tasks=addcontextmenufiles,addcontextmenufolders,addtopath'
+Start-Process -Filepath "$env:temp/myexe.exe" -ArgumentList $args
