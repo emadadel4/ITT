fa0b7175fae0f50956502d7c04012a99783fe0ba
Emad Adel <emadadel1920@gmail.com>
2024-02-01 05:57:14 +0200

    ITT

--- quotes.ps1
+++ quotes.ps1
@@ -1,9 +1,7 @@
-cls
-
 $q = "I Believe What Doesn't Kill You Simply Makes You... Stranger",
 "You Either Die A Hero Or Live Long Enough To See Yourself Become The Villain.",
 "Some Men Just Want To Watch The World Burn.",
 "I'm Batman.",
 "You Always Fear What You Don't Understand",
 "You thought we could be decent men, in an indecent time. But you were wrong."
-$q | Get-Random 
+$q | Get-Random
