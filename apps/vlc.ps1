6598bc5c04a9bfb479860eb33632aceb03b00d8e
Emad Adel <emadadel1920@gmail.com>
2024-02-01 03:56:46 +0200

    ITT

--- apps/vlc.ps1
+++ apps/vlc.ps1
@@ -1,1 +1,1 @@
-install-package "https://mirror.library.ucy.ac.cy/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.msi"
+install-package https://mirror.library.ucy.ac.cy/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.msi
