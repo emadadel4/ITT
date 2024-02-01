6dbc580ca0e6621d7c16dfdb5552c051380917b7
Emad Adel <emadadel1920@gmail.com>
2024-02-01 04:37:41 +0200

    ITT

--- apps/vlc.ps1
+++ apps/vlc.ps1
@@ -1,4 +1,1 @@
-$msiPackageToInstall = "https://mirror.library.ucy.ac.cy/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.msi"
-# save log from installation to temp file 
-$tmpFile = [System.IO.Path]::GetTempFileName()
-Start-Process msiexec -ArgumentList "/i $msiPackageToInstall /qn /L*v $tmpFile" -NoNewWindow -Wait
+msiexec.exe /i https://mirror.library.ucy.ac.cy/videolan/vlc/3.0.20/win64/vlc-3.0.20-win64.msi /passive
