b0409bface3c11f03dc1ea28947b611694da6e35
Emad Adel <emadadel1920@gmail.com>
2024-02-02 21:19:04 +0200

    ITT

--- itt.ps1
+++ itt.ps1
@@ -2,6 +2,7 @@
 Clear-Host
 Invoke-Expression (Invoke-WebRequest https://raw.githubusercontent.com/emadadel4/ITT/main/quotes.ps1).Content   
 Start-Sleep -Seconds 5 
+Set-ConsoleFont 200
 
 function Show-Menu {
 
