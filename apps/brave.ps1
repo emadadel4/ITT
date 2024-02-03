26774c4680ffba95d3910a077fb2fb545ebd5e1b
Emad Adel <emadadel1920@gmail.com>
2024-02-04 01:51:49 +0200

    Update brave.ps1

--- apps/brave.ps1
+++ apps/brave.ps1
@@ -1,3 +1,8 @@
 
-$brave = "https://referrals.brave.com/latest/BraveBrowserSetup-BRV029.exe"
-msiexec.exe /i $brave /passive
+$url = "https://referrals.brave.com/latest/BraveBrowserSetup-BRV029.exe"
+$outpath = "$PSScriptRoot/brave.exe"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+$args = @("Comma","Separated","Arguments")
+Start-Process -Filepath "$PSScriptRoot/brave.exe" -ArgumentList $args
