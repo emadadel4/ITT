930db88223bdb99d308db4016ff26221d34e6d04
Emad Adel <emadadel1920@gmail.com>
2024-02-04 01:49:16 +0200

    ITT

---
+++ apps/brave.ps1
@@ -0,0 +1,3 @@
+
+$brave = "https://referrals.brave.com/latest/BraveBrowserSetup-BRV029.exe"
+msiexec.exe /i $brave /passive
--- apps/testwpf.ps1
+++ apps/testwpf.ps1
@@ -2,7 +2,7 @@
 Add-Type -AssemblyName PresentationFramework
 
 # where is the XAML file?
-$xamlFile = "C:\Users\Emad Adel\source\repos\ITTSoft\ITTSoft\MainWindow.xaml"
+$xamlFile = "C:\Users\Emad Adel\source\repos\ITTS\ITTS\MainWindow.xaml"
 
 #create window
 $inputXML = Get-Content $xamlFile -Raw
@@ -18,17 +18,33 @@ try {
     throw
 }
 
-$PCNames = @("Firefox","Chrome","Edge")
-$listBox1 = $window.FindName("listbox")
+$listview = $window.FindName("listview")
 
-foreach ($checkBox in $PCNames) {
-    $listBox1.Items.Add( "$checkBox")
+$firefox = $window.FindName("firefox")
+$chrome = $window.FindName("chrome")
+$brave = $window.FindName("brave")
+
+$installbtn = $window.FindName("installbtn")
+
+
+$handler_button1_Click= 
+{
+    if ($firefox.IsChecked) 
+    { 
+        Invoke-Expression (Invoke-WebRequest https://raw.githubusercontent.com/emadadel4/ITT/main/apps/firefox.ps1).Content
+    }
+
+    if ($chrome.IsChecked) 
+    { 
+        Invoke-Expression (Invoke-WebRequest https://raw.githubusercontent.com/emadadel4/ITT/main/apps/chrome.ps1).Content
+    }
+
+    if ($brave.IsChecked) 
+    { 
+        Invoke-Expression (Invoke-WebRequest https://raw.githubusercontent.com/emadadel4/ITT/main/apps/brave.ps1).Content
+    }
 }
 
-$listBox1.MouseEnter({
-    
-})
-
-
+$installbtn.add_Click($handler_button1_Click)
 
 $Null = $window.ShowDialog()
