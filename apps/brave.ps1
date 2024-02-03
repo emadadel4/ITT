c6984b2d52894937c52d263a2da805fd592c6848
Emad Adel <emadadel1920@gmail.com>
2024-02-04 01:58:12 +0200

    ITT

--- apps/brave.ps1
+++ apps/brave.ps1
@@ -4,5 +4,6 @@ $outpath = "$env:temp/brave.exe"
 Invoke-WebRequest -Uri $url -OutFile $outpath
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
+
 $args = @("Comma","Separated","Arguments")
-Start-Process -Filepath ""$env:temp/brave.exe" -ArgumentList $args
+Start-Process -Filepath "$env:temp/brave.exe" -ArgumentList $args
