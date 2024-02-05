90705020974f5afad8557339b9a7772d6099d172
Emad Adel <emadadel1920@gmail.com>
2024-02-06 01:43:10 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -43,7 +43,7 @@ do {
 
         '3'
         {
-            Invoke-WebRequest "https://raw.githubusercontent.com/emadadel4/Fix-Stutter-in-Games/main/Fix%20Stutter%20Games%20%5Brun%20as%20administrator%5D.bat" -outfile "fix.bat"
+            Invoke-WebRequest "https://raw.githubusercontent.com/emadadel4/Fix-Stutter-in-Games/main/FixStutter.bat" -outfile "fix.bat"
             Start-Process -FilePath "fix.bat"
         }
        
