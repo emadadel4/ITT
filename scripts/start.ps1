4a10cf205c4705dbb81dbd579ea04b573016f913
Emad Adel <emadadel1920@gmail.com>
2024-03-25 03:45:26 +0200

    debug

---
+++ scripts/start.ps1
@@ -0,0 +1,65 @@
+<#
+.NOTES
+    Author         : Emad Adel @emadadel4
+    Runspace Author: @DeveloperDurp
+    GitHub         : https://github.com/emadadel4
+    Website        : https://eprojects.orgfree.com/
+    Version        : #{replaceme}
+#>
+param (
+    [switch]$Debug,
+    [string]$Config,
+    [switch]$Run
+)
+
+# Set DebugPreference based on the -Debug switch
+if ($Debug) {
+    $DebugPreference = "Continue"
+}
+
+if ($Config) {
+    $PARAM_CONFIG = $Config
+}
+
+$PARAM_RUN = $false
+# Handle the -Run switch
+if ($Run) {
+    Write-Host "Running config file tasks..."
+    $PARAM_RUN = $true
+}
+
+if (!(Test-Path -Path $ENV:TEMP)) {
+    New-Item -ItemType Directory -Force -Path $ENV:TEMP
+}
+
+Start-Transcript $ENV:TEMP\Winutil.log -Append
+
+# Load DLLs
+Add-Type -AssemblyName PresentationFramework
+Add-Type -AssemblyName System.Windows.Forms
+
+# Variable to sync between runspaces
+$sync = [Hashtable]::Synchronized(@{})
+$sync.PSScriptRoot = $PSScriptRoot
+$sync.version = "#{replaceme}"
+$sync.configs = @{}
+$sync.ProcessRunning = $false
+
+$currentPid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = new-object System.Security.Principal.WindowsPrincipal($currentPid)
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+
+if ($principal.IsInRole($adminRole))
+{
+    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Admin)"
+    clear-host
+}
+else
+{
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    $newProcess.Verb = "runas";
+    [System.Diagnostics.Process]::Start($newProcess);
+    break
+}
