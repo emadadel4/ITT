05f5cf1ff90e9006ace055ee4665df4cccd8f0d2
Emad Adel <emadadel1920@gmail.com>
2024-03-31 05:42:42 +0200

    ITT

--- functions/TapOnSelectionChanged.ps1
+++ functions/TapOnSelectionChanged.ps1
@@ -2,14 +2,12 @@ function ChangeTap() {
     
     if($window.FindName('apps').IsSelected)
     {
-        Write-Host "Apps"
         $window.FindName('installBtn').Visibility = "Visible"
         $window.FindName('applyBtn').Visibility = "Hidden"
     }
 
     if($window.FindName('tweeks').IsSelected)
     {
-        Write-Host "Tweaks"
         $window.FindName('applyBtn').Visibility = "Visible"
         $window.FindName('installBtn').Visibility = "Hidden"
     }
--- itt.ps1
+++ itt.ps1
@@ -315,14 +315,12 @@ function ChangeTap() {
     
     if($window.FindName('apps').IsSelected)
     {
-        Write-Host "Apps"
         $window.FindName('installBtn').Visibility = "Visible"
         $window.FindName('applyBtn').Visibility = "Hidden"
     }
 
     if($window.FindName('tweeks').IsSelected)
     {
-        Write-Host "Tweaks"
         $window.FindName('applyBtn').Visibility = "Visible"
         $window.FindName('installBtn').Visibility = "Hidden"
     }
