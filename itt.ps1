c86a0eeb21f92534db32a28c993526034934c4e5
Emad Adel <emadadel1920@gmail.com>
2024-04-03 05:30:16 +0200

    ITT

--- itt.ps1
+++ itt.ps1
@@ -314,12 +314,14 @@ function ChangeTap() {
     
     if($window.FindName('apps').IsSelected)
     {
+        $itemLink.Visibility = "Visible"
         $window.FindName('installBtn').Visibility = "Visible"
         $window.FindName('applyBtn').Visibility = "Hidden"
     }
 
     if($window.FindName('tweeks').IsSelected)
     {
+        $itemLink.Visibility = "Hidden"
         $window.FindName('applyBtn').Visibility = "Visible"
         $window.FindName('installBtn').Visibility = "Hidden"
     }
