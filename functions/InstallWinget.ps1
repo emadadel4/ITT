d4469a241169a620a26c12a03310171c089d926a
Emad Adel <emadadel1920@gmail.com>
2024-04-07 05:53:43 +0200

    ITT

--- functions/InstallWinget.ps1
+++ functions/InstallWinget.ps1
@@ -6,10 +6,8 @@ function Install-WinWinget {
     if ($wingetInstalled) {
         Write-Output "winget is installed."
     } else {
-
-        Install-Module -Name WingetTools
-        Install-WinGet
-        Write-Output "winget is not installed."
+        Install-Module -Name Microsoft.WinGet.Client -Force -AllowClobber
+        Write-Output "winget is not installed. gonna install it"
     }
 
 }
