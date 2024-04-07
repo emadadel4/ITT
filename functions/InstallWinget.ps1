3dab4d3350e997829d27d9d9af0d4b2675feafd0
Emad Adel <emadadel1920@gmail.com>
2024-04-07 02:12:43 +0200

    ITT

--- functions/InstallWinget.ps1
+++ functions/InstallWinget.ps1
@@ -2,16 +2,25 @@ function Install-WinWinget {
 
     # Check if winget is installed
     if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
+        # Download the installer
+        $url = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle"
+        $installerPath = "$env:TEMP\winget.appxbundle"
+        Invoke-WebRequest -Uri $url -OutFile $installerPath
 
-        irm https://github.com/asheroto/winget-install/releases/latest/download/winget-install.ps1 | iex
+        # Install winget
+        Add-AppxPackage -Path $installerPath
 
-        # Optional: Check if installation was successful
+        # Check if installation was successful
         if (Get-Command winget -ErrorAction SilentlyContinue) {
             Write-Output "winget installed successfully."
         } else {
-            Write-Output "winget installation failed."
+            Write-Output "Failed to install winget."
         }
+
+        # Clean up the installer
+        Remove-Item $installerPath -Force
     } else {
         Write-Output "winget is already installed."
     }
+
 }
