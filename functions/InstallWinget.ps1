3383b1cc4271ef8f417fe092b5e182edb63e699c
Emad Adel <emadadel1920@gmail.com>
2024-04-07 02:02:44 +0200

    ITT

--- functions/InstallWinget.ps1
+++ functions/InstallWinget.ps1
@@ -1,26 +1,17 @@
 function Install-WinWinget {
 
     # Check if winget is installed
-    if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
-        # Install winget
-        Write-Output "winget is not installed. Trying to install..."
-        
-        # Check if PowerShellGet module is installed
-        if (!(Get-Module -Name PowerShellGet -ListAvailable)) {
-            Write-Output "PowerShellGet module not found. Installing PowerShellGet module..."
-            Install-Module -Name PowerShellGet -Force -AllowClobber -Scope CurrentUser -Repository PSGallery -ErrorAction SilentlyContinue
-        }
+    if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
 
-        # Install winget
-        Install-Module -Name winget -Force -AllowClobber -Scope CurrentUser -Repository PSGallery -ErrorAction SilentlyContinue
+        irm https://github.com/asheroto/winget-install/releases/latest/download/winget-install.ps1 | iex
 
-        if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
-            Write-Output "Failed to install winget. Please download and install it manually."
-        } else {
+        # Optional: Check if installation was successful
+        if (Get-Command winget -ErrorAction SilentlyContinue) {
             Write-Output "winget installed successfully."
+        } else {
+            Write-Output "winget installation failed."
         }
     } else {
         Write-Output "winget is already installed."
     }
- 
 }
