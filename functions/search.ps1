0fabc0674a3f350bbefb4cf95a8df45c470d8d94
Emad Adel <emadadel1920@gmail.com>
2024-04-09 03:16:20 +0200

    Update search.ps1

--- functions/search.ps1
+++ functions/search.ps1
@@ -1,6 +1,8 @@
-#region Search in listview 
+#region Function to filter a list based on a search input
+
 function Search{
     
+    # Retrieves the search input, converts it to lowercase, and filters the list based on the input
     $filter = $window.FindName('searchInput').Text.ToLower()
     $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($list.Items)
     $collectionView.Filter = {
