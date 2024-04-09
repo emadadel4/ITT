6668ff13c058091245b1f2de9f65eeb7355aa3ab
Emad Adel <emadadel1920@gmail.com>
2024-04-09 11:05:59 +0200

    Clean design

--- itt.ps1
+++ itt.ps1
@@ -1498,7 +1498,7 @@ $inputXML =  '
 
         <!--Footer-->
         <Grid Grid.Row="2">
-                <TextBlock Cursor="Pen" x:Name="quotes"  HorizontalAlignment="Left" VerticalAlignment="Center" Padding="16" TextWrapping="Wrap" Text="?????? ???????????? ???? ???????? ?????????? ???? ??????????" Foreground="Black"/>
+                <TextBlock Cursor="Pen" x:Name="quotes"  HorizontalAlignment="Left" VerticalAlignment="Center" Padding="16" TextWrapping="Wrap" Text="When I finished building my boat, the sea dried up." Foreground="Black"/>
             </Grid>
         <!--End Footer-->
 
