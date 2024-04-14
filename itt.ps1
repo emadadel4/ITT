3ef633a21c17e49795856a0a92be3fa9f1fc5324
Emad Adel <emadadel1920@gmail.com>
2024-04-14 07:06:40 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -1605,30 +1605,6 @@ catch [System.Management.Automation.MethodInvocationException] {
 
     #endregion
 
-# Check if the JSON content is an array
-if ($sync.configs.quotes -is [array])
-{
-    # Select a random index
-    $randomIndex = Get-Random -Minimum 0 -Maximum ($sync.configs.quotes.Count)
-
-    # Get the random text
-    $randomText = $jsonContent[$randomIndex]
-
-    # Output the random text
-    $Window.FindName('quotes').Text = $randomText
-
-}
-
-#Install Choco
-CheckChoco
-
-#===========================================================================
-# End Loops 
-#===========================================================================
-
-
-    
-
 #===========================================================================
 # Events 
 #===========================================================================
