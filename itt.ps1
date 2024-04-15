6cdcafc44b976328f405eed91cde5a1e5d0a7ed5
Emad Adel <emadadel1920@gmail.com>
2024-04-15 04:07:04 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -106,6 +106,39 @@ function ShowAll{
             $checkbox.Foreground = "#b3b3b3"
     }
 }
+
+function CheckChoco {
+
+    try {
+
+        if((Get-Command -Name choco -ErrorAction Ignore))
+        {
+Write-Host 
+"
+    ___ _____ _____     ____  _____ __  __    _    ____       _    ____  _____ _     
+    |_ _|_   _|_   _|   / __ \| ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |    
+    | |  | |   | |    / / _` |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |    
+    | |  | |   | |   | | (_| | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___ 
+    |___| |_|   |_|    \ \__,_|_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|
+                        \____/                                                        
+
+                    This is Emad adel an Binary
+    01000101 01001101 01000001 01000100  01000001 01000100 01000101 01001100      
+                Chocolatey is installed You Good to go                  
+"
+            return
+        }
+
+        Write-Host "Chocolatey is not installed, installing now"
+        Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
+        powershell choco feature enable -n allowGlobalConfirmation
+    }
+    Catch {
+        Write-Host "--Chocolatey failed to install---"
+    }
+}
+
+
 # Create a runspace to execute Winget command
 $runspace = [runspacefactory]::CreateRunspace()
 $runspace.Open()
@@ -259,39 +292,6 @@ function ApplyTweaks() {
         [System.Windows.MessageBox]::Show("You have to select", "ITT @emadadel", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Question)
     }
 }
-
-
-function CheckChoco {
-
-    try {
-
-        if((Get-Command -Name choco -ErrorAction Ignore))
-        {
-Write-Host 
-"
-    ___ _____ _____     ____  _____ __  __    _    ____       _    ____  _____ _     
-    |_ _|_   _|_   _|   / __ \| ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |    
-    | |  | |   | |    / / _` |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |    
-    | |  | |   | |   | | (_| | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___ 
-    |___| |_|   |_|    \ \__,_|_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|
-                        \____/                                                        
-
-                    This is Emad adel an Binary
-    01000101 01001101 01000001 01000100  01000001 01000100 01000101 01001100      
-                Chocolatey is installed You Good to go                  
-"
-            return
-        }
-
-        Write-Host "Chocolatey is not installed, installing now"
-        Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
-        powershell choco feature enable -n allowGlobalConfirmation
-    }
-    Catch {
-        Write-Host "--Chocolatey failed to install---"
-    }
-}
-
 
 function Recommended() {
 
