d629737f4d37a48c103a3c49499682d90be2b467
Emad Adel <emadadel1920@gmail.com>
2024-04-16 08:20:00 +0200

    Switch back to apps tab when click on any catgory

--- Functions/Filter-Category.ps1
+++ Functions/Filter-Category.ps1
@@ -40,3 +40,24 @@ function ShowAll{
             $checkbox.Foreground = "White"
     }
 }
+
+function Recommended() {
+
+    $window.FindName('apps').IsSelected = $true 
+
+    #Clear Listview
+    $list.Items.Clear()
+
+    # get items that has check = true in json file
+    foreach ($item in $sync.configs.applications)
+    {
+        # $item.check = true
+        if($item.check -eq 'true')
+        {
+            $checkbox = New-Object System.Windows.Controls.CheckBox
+            $list.Items.Add($checkbox)
+            $checkbox.Content = $item.name
+            $checkbox.Foreground = "White"
+        }
+    }
+}
--- itt.ps1
+++ itt.ps1
@@ -109,6 +109,27 @@ function ShowAll{
     }
 }
 
+function Recommended() {
+
+    $window.FindName('apps').IsSelected = $true 
+
+    #Clear Listview
+    $list.Items.Clear()
+
+    # get items that has check = true in json file
+    foreach ($item in $sync.configs.applications)
+    {
+        # $item.check = true
+        if($item.check -eq 'true')
+        {
+            $checkbox = New-Object System.Windows.Controls.CheckBox
+            $list.Items.Add($checkbox)
+            $checkbox.Content = $item.name
+            $checkbox.Foreground = "White"
+        }
+    }
+}
+
 function CheckChoco 
 {
 
@@ -291,24 +312,6 @@ function ApplyTweaks() {
     }
 }
 
-function Recommended() {
-
-    #Clear Listview
-    $list.Items.Clear()
-
-    # get items that has check = true in json file
-    foreach ($item in $sync.configs.applications)
-    {
-        # $item.check = true
-        if($item.check -eq 'true')
-        {
-            $checkbox = New-Object System.Windows.Controls.CheckBox
-            $list.Items.Add($checkbox)
-            $checkbox.Content = $item.name
-            $checkbox.Foreground = "White"
-        }
-    }
-}
 #region Function to filter a list based on a search input
 
 function Search{
