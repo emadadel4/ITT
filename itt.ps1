be964b61ca97c92aa635afb5ccb00bc2aad56280
Emad Adel <emadadel1920@gmail.com>
2024-04-17 08:38:13 +0200

    Play random music

--- itt.ps1
+++ itt.ps1
@@ -364,9 +364,10 @@ function PlayMusic {
     Invoke-RunspaceWithScriptBlock -ScriptBlock {
 
         $audioUrls = @(
-        "https://epsilon.vgmsite.com/soundtracks/far-cry-3/iqgdbfrhtw/17.%20Further%20%28feat.%20Serena%20McKinney%29.mp3",
-        "https://vgmsite.com/soundtracks/assassins-creed-ezios-family-m-me-remix-2022/qdxeshajdz/01.%20Ezio%27s%20Family%20%28M%C3%B8me%20Remix%29.mp3"
-
+            "https://epsilon.vgmsite.com/soundtracks/far-cry-3/iqgdbfrhtw/17.%20Further%20%28feat.%20Serena%20McKinney%29.mp3",
+            "https://dl.vgmdownloads.com/soundtracks/hollow-knight-original-soundtrack/qqrmmaqyqg/26.%20Hollow%20Knight.mp3",
+            " https://dl.vgmdownloads.com/soundtracks/assassins-creed-mirage-original-game-soundtrack-2023/axtwruyduh/01.%20Mirage%20Theme.mp3",
+            "https://vgmsite.com/soundtracks/assassins-creed-ezios-family-m-me-remix-2022/qdxeshajdz/01.%20Ezio%27s%20Family%20%28M%C3%B8me%20Remix%29.mp3"
         )
     
         $mediaPlayer = New-Object -ComObject WMPlayer.OCX
@@ -383,10 +384,9 @@ function PlayMusic {
     
         # Function to play the entire playlist
         Function PlayPlaylist {
-            foreach ($url in $audioUrls) {
-                PlayAudio $url
-                
-            }
+            $randomIndex = Get-Random -Minimum 0 -Maximum $audioUrls.Count
+            $randomUrl = $audioUrls[$randomIndex]
+            PlayAudio $randomUrl
         }
     
         # Play the playlist indefinitely
