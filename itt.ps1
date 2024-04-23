b95f796200c1559143fdb9376ad8fa3c2d2776d6
Emad Adel <emadadel1920@gmail.com>
2024-04-23 10:56:12 +0200

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -381,69 +381,74 @@ function Invoke-RunspaceWithScriptBlock {
 }
 
 
-# function PlayMusic {
+function PlayMusic {
 
-#     Invoke-RunspaceWithScriptBlock -ScriptBlock {
+    Invoke-RunspaceWithScriptBlock -ScriptBlock {
 
-#         $audioUrls = @(
-#             "https://epsilon.vgmsite.com/soundtracks/far-cry-3/iqgdbfrhtw/17.%20Further%20%28feat.%20Serena%20McKinney%29.mp3",
-#             "https://dl.vgmdownloads.com/soundtracks/hollow-knight-original-soundtrack/qqrmmaqyqg/26.%20Hollow%20Knight.mp3",
-#             "https://dl.vgmdownloads.com/soundtracks/assassin-s-creed-unity-vol.-1/hxqrvcoyfj/01.%20Unity.mp3",
-#             "https://dl.vgmdownloads.com/soundtracks/assassin-s-creed-3/jgevpclfcr/01.%20Assassin%27s%20Creed%20III%20Main%20Theme.mp3",
-#             "https://dl.vgmdownloads.com/soundtracks/assassins-creed-mirage-original-game-soundtrack-2023/axtwruyduh/01.%20Mirage%20Theme.mp3",
-#             "https://vgmsite.com/soundtracks/assassins-creed-ezios-family-m-me-remix-2022/qdxeshajdz/01.%20Ezio%27s%20Family%20%28M%C3%B8me%20Remix%29.mp3",
-#             "https://epsilon.vgmsite.com/soundtracks/assassin-s-creed-iv-black-flag/zxpesokhkg/1-02%20Pyrates%20Beware.mp3",
-#             "https://vgmsite.com/soundtracks/battlefield-3/tabqykkp/01.%20Battlefield%203%20Main%20Theme.mp3",
-#             "https://archive.org/download/GrandTheftAuto4ThemeSong_201904/Grand%20Theft%20Auto%204%20Theme%20Song.mp3"
-#         )
+        $audioUrls = @(
+            "https://epsilon.vgmsite.com/soundtracks/far-cry-3/iqgdbfrhtw/17.%20Further%20%28feat.%20Serena%20McKinney%29.mp3",
+            "https://dl.vgmdownloads.com/soundtracks/hollow-knight-original-soundtrack/qqrmmaqyqg/26.%20Hollow%20Knight.mp3",
+            "https://dl.vgmdownloads.com/soundtracks/assassin-s-creed-unity-vol.-1/hxqrvcoyfj/01.%20Unity.mp3",
+            "https://dl.vgmdownloads.com/soundtracks/assassin-s-creed-3/jgevpclfcr/01.%20Assassin%27s%20Creed%20III%20Main%20Theme.mp3",
+            "https://dl.vgmdownloads.com/soundtracks/assassins-creed-mirage-original-game-soundtrack-2023/axtwruyduh/01.%20Mirage%20Theme.mp3",
+            "https://vgmsite.com/soundtracks/assassins-creed-ezios-family-m-me-remix-2022/qdxeshajdz/01.%20Ezio%27s%20Family%20%28M%C3%B8me%20Remix%29.mp3",
+            "https://epsilon.vgmsite.com/soundtracks/assassin-s-creed-iv-black-flag/zxpesokhkg/1-02%20Pyrates%20Beware.mp3",
+            "https://vgmsite.com/soundtracks/battlefield-3/tabqykkp/01.%20Battlefield%203%20Main%20Theme.mp3",
+            "https://archive.org/download/GrandTheftAuto4ThemeSong_201904/Grand%20Theft%20Auto%204%20Theme%20Song.mp3",
+            "https://epsilon.vgmsite.com/soundtracks/assassin-s-creed-2/jlrprchapt/1-03%20Ezio%27s%20Family.mp3",
+            "https://epsilon.vgmsite.com/soundtracks/assassin-s-creed-2/nkantwuktr/1-01%20Earth.mp3",
+            "https://vgmsite.com/soundtracks/mass-effect-gamerip-2007/widiaiov/37.%20Spectre%20Induction.mp3",
+            "https://epsilon.vgmsite.com/soundtracks/mass-effect-3-gamerip-2012/nchtmgcz/304.%20End%20of%20Cycle.mp3"
+            
+        )
     
-#         $global:mediaPlayer = New-Object -ComObject WMPlayer.OCX
-#         $global:playlistPaused = $false
+        $global:mediaPlayer = New-Object -ComObject WMPlayer.OCX
+        $global:playlistPaused = $false
 
-#         Function PlayAudio($url) {
-#             try {
-#                 $mediaItem = $global:mediaPlayer.newMedia($url)
-#                 $global:mediaPlayer.currentPlaylist.appendItem($mediaItem)
-#                 $global:mediaPlayer.controls.play()
-#             }
-#             catch {
-#             }
-#         }
+        Function PlayAudio($url) {
+            try {
+                $mediaItem = $global:mediaPlayer.newMedia($url)
+                $global:mediaPlayer.currentPlaylist.appendItem($mediaItem)
+                $global:mediaPlayer.controls.play()
+            }
+            catch {
+            }
+        }
         
     
-#         # Function to shuffle the playlist
-#         Function ShuffleArray {
-#             param([array]$array)
-#             $count = $array.Length
-#             for ($i = 0; $i -lt $count; $i++) {
-#                 $randomIndex = Get-Random -Minimum $i -Maximum $count
-#                 $temp = $array[$i]
-#                 $array[$i] = $array[$randomIndex]
-#                 $array[$randomIndex] = $temp
-#             }
-#         }
+        # Function to shuffle the playlist
+        Function ShuffleArray {
+            param([array]$array)
+            $count = $array.Length
+            for ($i = 0; $i -lt $count; $i++) {
+                $randomIndex = Get-Random -Minimum $i -Maximum $count
+                $temp = $array[$i]
+                $array[$i] = $array[$randomIndex]
+                $array[$randomIndex] = $temp
+            }
+        }
     
-#         # Shuffle the playlist
-#         ShuffleArray -array $audioUrls
+        # Shuffle the playlist
+        ShuffleArray -array $audioUrls
     
-#         # Function to play the entire shuffled playlist
-#         Function PlayShuffledPlaylist {
-#             foreach ($url in $audioUrls) {
-#                 PlayAudio $url
-#                 # Wait for the track to finish playing
-#                 while ($global:mediaPlayer.playState -eq 3 -or $global:mediaPlayer.playState -eq 6) {
-#                     Start-Sleep -Milliseconds 100
-#                 }
-#             }
-#         }
+        # Function to play the entire shuffled playlist
+        Function PlayShuffledPlaylist {
+            foreach ($url in $audioUrls) {
+                PlayAudio $url
+                # Wait for the track to finish playing
+                while ($global:mediaPlayer.playState -eq 3 -or $global:mediaPlayer.playState -eq 6) {
+                    Start-Sleep -Milliseconds 100
+                }
+            }
+        }
     
-#         # Play the shuffled playlist indefinitely
-#         while ($true) 
-#         {
-#             PlayShuffledPlaylist
-#         }
-#     }
-# }
+        # Play the shuffled playlist indefinitely
+        while ($true) 
+        {
+            PlayShuffledPlaylist
+        }
+    }
+}
 
 function GetQuotes {
 
@@ -1925,7 +1930,7 @@ $sync.configs.Quotes = '{
     "الأصدقاء كالنجوم، لا تراها دوماً ولكنك تعلم أنها موجودة",
     "النية خير معيار للأفعال",
     "الإيمان هو أن ترى الشيء الذي لا يمكن رؤيته بالعين",
-    "الحقيقة تجعلك حرًا.",
+    "الحقيقة تجعلك حرًا",
     "الحياة قصيرة، استخدمها بحكمة"
   ]
 }
@@ -2342,7 +2347,7 @@ $inputXML =  '
                 <!--Catagory Section-->
                     <StackPanel Name="catg" Margin="20,0,0,0" Orientation="Horizontal" HorizontalAlignment="Left">
 
-                    <ComboBox SelectedIndex="0"  Margin="10,10,0,13" Name="emad" HorizontalAlignment="Left" VerticalAlignment="Top" Width="Auto" Height="Auto">
+                    <ComboBox SelectedIndex="0"  Margin="10,10,0,13" Name="cat" HorizontalAlignment="Left" VerticalAlignment="Top" Width="Auto" Height="Auto">
                         
                         
 
@@ -2679,7 +2684,6 @@ CheckChoco
 #===========================================================================
 # Events 
 #===========================================================================
-
 # Buttons
 $sync['window'].FindName('taps').add_SelectionChanged({ChangeTap})
 $sync['window'].FindName('installBtn').add_click({Invoke-Install})
@@ -2688,16 +2692,21 @@ $sync['window'].FindName('searchInput').add_TextChanged({Search})
 $sync['window'].FindName('searchInput').add_GotFocus({ClearFilter})
 $sync['window'].FindName('about').add_MouseLeftButtonDown({About})
 $sync['window'].FindName('themeText').add_click({Toggle-Theme})
-
-$sync['window'].FindName('emad').add_SelectionChanged({
-
-  FilterByCat( $sync['window'].FindName('emad').SelectedItem.Content)
-
-})
-
-Switch-ToDarkMode
+$sync['window'].FindName('cat').add_SelectionChanged({FilterByCat( $sync['window'].FindName('cat').SelectedItem.Content)})
+#===========================================================================
+# End Events 
+#===========================================================================
 
 GetQuotes *> $null
+PlayMusic *> $null
+
+
+$sync['window'].Add_Closing({
+
+  Write-Host "Bye see you soon :)"
+
+  Stop-Process -Id $PID
+})
 
 if ($global:themePreference -eq "Dark") {
   Switch-ToDarkMode
@@ -2707,11 +2716,5 @@ if ($global:themePreference -eq "Dark") {
   # Default to light mode if preference not found
   Switch-ToLightMode
 }
-
-
-#===========================================================================
-# End Events 
-#===========================================================================
-
 
 $sync["window"].ShowDialog() | out-null
