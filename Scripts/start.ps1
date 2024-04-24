ac923e329d559eef1e84d89bceb6d8eed05ae243
Emad Adel <emadadel1920@gmail.com>
2024-04-25 00:41:14 +0200

    Update start.ps1

--- Scripts/start.ps1
+++ Scripts/start.ps1
@@ -19,6 +19,7 @@ Add-Type -AssemblyName PresentationFramework.Aero
 
 # Variable to sync between runspaces
 $sync = [Hashtable]::Synchronized(@{})
+$script:powershell = [powershell]::Create()
 $sync.PSScriptRoot = $PSScriptRoot
 $sync.version = "#{replaceme}"
 $sync.github = "https://github.com/emadadel4"
