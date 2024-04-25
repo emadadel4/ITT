dd391b04e04b4794fdd7ae8455b6b0b258364ab4
Emad Adel <emadadel1920@gmail.com>
2024-04-25 10:45:44 +0200

    Update Playlist.ps1

--- Functions/Playlist.ps1
+++ Functions/Playlist.ps1
@@ -15,7 +15,9 @@ $audioUrls = @(
     "https://archive.org/download/GrandTheftAuto4ThemeSong_201904/Grand%20Theft%20Auto%204%20Theme%20Song.mp3",
     "https://epsilon.vgmsite.com/soundtracks/assassin-s-creed-2/jlrprchapt/1-03%20Ezio%27s%20Family.mp3",
     "https://epsilon.vgmsite.com/soundtracks/assassin-s-creed-2/nkantwuktr/1-01%20Earth.mp3",
-    "https://epsilon.vgmsite.com/soundtracks/mass-effect-3-gamerip-2012/nchtmgcz/304.%20End%20of%20Cycle.mp3"
+    "https://epsilon.vgmsite.com/soundtracks/mass-effect-3-gamerip-2012/nchtmgcz/304.%20End%20of%20Cycle.mp3",
+    "https://archive.org/download/Cairokee-Roma/07%20-%20Roma.mp3",
+    "https://archive.org/download/Cairokee-Roma/01%20-%20Matwhashneesh.mp3"
 )
 
 Function PlayAudio($url) {
