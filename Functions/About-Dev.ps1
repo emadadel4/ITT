2069114c6d7f8ad18f51bbd87a4aec96fdf97d30
Emad Adel <emadadel1920@gmail.com>
2024-04-26 17:23:16 +0300

    Update About-Dev.ps1

--- Functions/About-Dev.ps1
+++ Functions/About-Dev.ps1
@@ -6,10 +6,11 @@
 function About{
 
     $authorInfo = @"
-        Author   : $($sync.author)
-        GitHub   : $($sync.github)
-        Website  : $($sync.website)
-        Version  : $($sync.version)
+    Developer : $($sync.developer)
+    Telegram  : $($sync.telegram)
+    GitHub    : $($sync.github)
+    Website   : $($sync.website)
+    Version   : $($sync.version)
 "@
 
     Show-CustomDialog -Message $authorInfo -Width 400 
