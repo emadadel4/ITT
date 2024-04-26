a90630a09c848f57009f3a26120e0333519133a1
Emad Adel <emadadel1920@gmail.com>
2024-04-26 17:23:29 +0300

    Update Install-Choco.ps1

--- Functions/Install-Choco.ps1
+++ Functions/Install-Choco.ps1
@@ -7,21 +7,26 @@ function CheckChoco
     if((Get-Command -Name choco -ErrorAction Ignore))
     {
  
-        echo "  ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _    _  _   ";
-        echo " |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |  | || |  ";
-        echo "  | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |  | || |_ ";
-        echo "  | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |__|__   _|";
-        echo " |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____| |_|  ";
-        echo "                                                                                 ";
-        
-        echo "This is Emad adel an Binary ";
-        echo "01000101 01001101 01000001 01000100  01000001 01000100 01000101 01001100      ";
-        echo "Chocolatey is installed You Good to go  ";
-                               
+Write-Host "
+___ _____ _____   _____ __  __    _    ____    _    ____  _____ _    _  _   
+|_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \  / \  |  _ \| ____| |  | || |  
+ | |  | |   | |   |  _| | |\/| | / _ \ | | | |/ _ \ | | | |  _| | |  | || |_ 
+ | |  | |   | |   | |___| |  | |/ ___ \| |_| / ___ \| |_| | |___| |__|__   _|
+|___| |_|   |_|   |_____|_|  |_/_/   \_\____/_/   \_\____/|_____|_____| |_|  
+Chocolatey is installed You Good to go
+" -ForegroundColor green
         return
         }
 
-        Write-Host "Chocolatey is not installed, installing now"
+Write-Host "
+__        _______ _     ____ ___  __  __ _____   _____ ___    ___ _____ _____ 
+\ \      / / ____| |   / ___/ _ \|  \/  | ____| |_   _/ _ \  |_ _|_   _|_   _|
+ \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|     | || | | |  | |  | |   | |  
+  \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| |  | |  | |   | |  
+   \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/  |___| |_|   |_|  
+" -ForegroundColor White             
+Write-Host "Chocolatey is not installed, installing now" -ForegroundColor red             
+        
         Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop
         powershell choco feature enable -n allowGlobalConfirmation
     }
