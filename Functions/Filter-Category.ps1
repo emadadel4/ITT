aaac8a0cfba7cb305bb5e9af0baef1c261c66104
Emad Adel <emadadel1920@gmail.com>
2024-04-26 17:23:23 +0300

    Update Filter-Category.ps1

--- Functions/Filter-Category.ps1
+++ Functions/Filter-Category.ps1
@@ -1,58 +1,58 @@
 #region Function to filter a list based on a search input
-    function Search{
-        
-        # Retrieves the search input, converts it to lowercase, and filters the list based on the input
-        $filter = $sync['window'].FindName('searchInput').Text.ToLower()
-        $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('list').Items)
-        $collectionView.Filter = {
-            param($item)
-            $item -like "*$filter*"
-        }
-
+function Search{
+    
+    # Retrieves the search input, converts it to lowercase, and filters the list based on the input
+    $filter = $sync['window'].FindName('searchInput').Text.ToLower()
+    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('list').Items)
+    $collectionView.Filter = {
+        param($item)
+        $item -like "*$filter*"
     }
 
-    function FilterByCat {
-        param (
-            $Cat
-        )
+}
 
-        $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('list').Items)
+function FilterByCat {
+    param (
+        $Cat
+    )
 
-        # Define the filter predicate
-        $filterPredicate = {
-            param($item)
+    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('list').Items)
 
-            # Define the tag you want to filter by
-            $tagToFilter =  $Cat
-            # Check if the item has the tag
-            $itemTag = $item.Tag
-            return $itemTag -eq $tagToFilter
-        }
+    # Define the filter predicate
+    $filterPredicate = {
+        param($item)
 
-
-        if($Cat -eq "All")
-        {
-
-            $sync['window'].FindName('list').Clear()
-            $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('list').Items)
-            $collectionView.Filter = $null
-            
-        }else{
-
-            $sync['window'].FindName('list').Clear()
-            # Apply the filter to the collection view
-            $collectionView.Filter = $filterPredicate
-
-        }
-
-
-        
+        # Define the tag you want to filter by
+        $tagToFilter =  $Cat
+        # Check if the item has the tag
+        $itemTag = $item.Tag
+        return $itemTag -eq $tagToFilter
     }
 
-    function ClearFilter {
+
+    if($Cat -eq "All")
+    {
 
         $sync['window'].FindName('list').Clear()
         $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('list').Items)
         $collectionView.Filter = $null
+        
+    }else{
+
+        $sync['window'].FindName('list').Clear()
+        # Apply the filter to the collection view
+        $collectionView.Filter = $filterPredicate
+
     }
+
+
+    
+}
+
+function ClearFilter {
+
+    $sync['window'].FindName('list').Clear()
+    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('list').Items)
+    $collectionView.Filter = $null
+}
 #endregion
