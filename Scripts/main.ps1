7118acbba0ce2a0eb2e3ed64ab2929b3a291fd47
Wh0ami010 <167635892+Wh0ami010@users.noreply.github.com>
2024-04-27 16:11:52 +0200

    Add Computer Managment tools

--- Scripts/main.ps1
+++ Scripts/main.ps1
@@ -22,6 +22,24 @@ $sync['window'].FindName('searchInput').add_TextChanged({Search})
 $sync['window'].FindName('searchInput').add_GotFocus({ClearFilter})
 $sync['window'].FindName('about').add_MouseLeftButtonDown({About})
 $sync['window'].FindName('themeText').add_click({Toggle-Theme})
+
+
+$sync['window'].FindName('deviceManager').add_click({Start-Process devmgmt.msc})
+$sync['window'].FindName('services').add_click({Start-Process services.msc})
+$sync['window'].FindName('network').add_click({Start-Process ncpa.cpl})
+$sync['window'].FindName('sysinfo').add_click({Start-Process msinfo32.exe; dxdiag.exe})
+$sync['window'].FindName('poweroption').add_click({Start-Process powercfg.cpl})
+$sync['window'].FindName('appsfeatures').add_click({start-Process ms-settings:appsfeatures})
+
+$sync['window'].FindName('taskmgr').add_click({Start-Process taskmgr.exe})
+
+$sync['window'].FindName('diskmgmt').add_click({Start-Process diskmgmt.msc})
+
+
+
+
+
+
 $sync['window'].FindName('cat').add_SelectionChanged({FilterByCat( $sync['window'].FindName('cat').SelectedItem.Content)})
 $sync['window'].FindName('save').add_click({
   
