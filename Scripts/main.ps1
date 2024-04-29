4afecbd18057cf3498bee44793fa53211c682a27
Emad Adel <emadadel1920@gmail.com>
2024-04-30 01:40:19 +0300

    Update main.ps1

--- Scripts/main.ps1
+++ Scripts/main.ps1
@@ -1,5 +1,9 @@
 
-#check currnet Theme
+CheckChoco
+GetQuotes *> $null
+PlayMusic *> $null
+
+#region check currnet Theme
 if ($sync.theme -eq "Dark") {
   Switch-ToDarkMode
   $sync.isDarkMode = "Dark"
@@ -9,8 +13,9 @@ else
   Switch-ToLightMode
   $sync.isDarkMode = "Light"
 }
+#endregion
 
-# Buttons
+#region Buttons
 $sync['window'].FindName('taps').add_SelectionChanged({ChangeTap})
 $sync['window'].FindName('installBtn').add_click({Invoke-Install})
 $sync['window'].FindName('applyBtn').add_click({ApplyTweaks})
@@ -19,8 +24,9 @@ $sync['window'].FindName('searchInput').add_GotFocus({ClearFilter})
 $sync['window'].FindName('about').add_MouseLeftButtonDown({About})
 $sync['window'].FindName('themeText').add_click({ToggleTheme})
 $sync['window'].FindName('cat').add_SelectionChanged({FilterByCat( $sync['window'].FindName('cat').SelectedItem.Content)})
+#endregion Buttons
 
-# Computer Managment tools
+#region Computer Managment tools
 $sync['window'].FindName('deviceManager').add_click({Start-Process devmgmt.msc})
 $sync['window'].FindName('services').add_click({Start-Process services.msc})
 $sync['window'].FindName('network').add_click({Start-Process ncpa.cpl})
@@ -29,9 +35,9 @@ $sync['window'].FindName('poweroption').add_click({Start-Process powercfg.cpl})
 $sync['window'].FindName('appsfeatures').add_click({start-Process ms-settings:appsfeatures})
 $sync['window'].FindName('taskmgr').add_click({Start-Process taskmgr.exe})
 $sync['window'].FindName('diskmgmt').add_click({Start-Process diskmgmt.msc})
-# Computer Managment tools
+#endregion Computer Managment tools
 
-# Third-Party
+#region Third-Party
 $sync['window'].FindName('mas').add_click({
   Start-Process ("https://github.com/massgravel/Microsoft-Activation-Scripts")
 })
@@ -40,10 +46,31 @@ $sync['window'].FindName('idm').add_click({
 
 Start-Process ("https://github.com/WindowsAddict/IDM-Activation-Script")
 })
-# Third-Party
+#endregion Third-Party
 
+#region About
 
-# Preferences
+$sync['window'].FindName('ittlink').add_click({
+
+  Start-Process ("https://github.com/emadadel4/ITT")
+
+})
+
+$sync['window'].FindName('eprojectslink').add_click({
+
+  Start-Process ("https://eprojects.orgfree.com/")
+
+})
+
+$sync['window'].FindName('teleegramprofile').add_click({
+
+  Start-Process ("https://t.me/emadadel4")
+
+})
+
+#endregion About
+
+#region Preferences
 $sync['window'].FindName('load').add_click({LoadJson})
 $sync['window'].FindName('save').add_click({
   
@@ -51,17 +78,13 @@ $sync['window'].FindName('save').add_click({
   SaveItemsToJson
 
 })
-# Preferences
-
+#endregion Preferences
 
 $sync['window'].add_Closing({
   Write-Host "Bye see you soon :)"
-  StopMusic
   StopAllRunspace
+  StopMusic
 })
 
-CheckChoco
-GetQuotes *> $null
-PlayMusic *> $null
 
 $sync["window"].ShowDialog() | out-null
