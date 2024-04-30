553a820c65fd9973a31f14b347b0387ae0ca01f6
Emad Adel <emadadel1920@gmail.com>
2024-04-30 22:37:15 +0300

    Update Invoke-Button.ps1

--- Functions/Invoke-Button.ps1
+++ Functions/Invoke-Button.ps1
@@ -21,6 +21,14 @@ function Invoke-Button {
         "ittlink" { Start-Process ("https://github.com/emadadel4/ITT") $Button}
         "eprojectslink" { Start-Process ("https://eprojects.orgfree.com/") $Button}
         "teleegramprofile" {Start-Process ("https://t.me/emadadel4") $Button}
+        "deviceManager" {Start-Process devmgmt.msc $Button}
+        "appsfeatures" {Start-Process ms-settings:appsfeatures $Button}
+        "sysinfo" {Start-Process msinfo32.exe; dxdiag.exe; $Button}
+        "poweroption" {Start-Process powercfg.cpl $Button}
+        "services" {Start-Process services.msc $Button}
+        "network" {Start-Process ncpa.cpl $Button}
+        "taskmgr" {Start-Process taskmgr.exe $Button}
+        "diskmgmt" {Start-Process diskmgmt.msc $Button}
         "window" { StopAllRunspace Write-Host "Bye see you soon. :)" $Button }
     }
 }
