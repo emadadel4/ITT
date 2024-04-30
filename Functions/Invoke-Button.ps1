93a0066e1e3545ae8405743bab3f9a7447ced8c2
Emad Adel <emadadel1920@gmail.com>
2024-04-30 12:51:51 +0300

    Update Invoke-Button.ps1

--- Functions/Invoke-Button.ps1
+++ Functions/Invoke-Button.ps1
@@ -3,10 +3,27 @@ function Invoke-Button {
     Param ([string]$Button)
 
     # debug
-    #Write-Host $Button
+    Write-Host $Button
 
     Switch -Wildcard ($Button){
 
-        "installBtn" {ToggleTheme $Button}
+        "installBtn" {Invoke-Install $Button}
+        "applyBtn" {Invoke-ApplyTweaks $Button}
+        "taps" {ChangeTap $Button}
+        "load" {LoadJson $Button}
+        "save" {SaveItemsToJson $Button}
+        "themeText" {ToggleTheme $Button}
+        "searchInput" {Search $Button}
+        "about" {About $Button}
+        "cat" {FilterByCat($sync.cat.SelectedItem.Content) $Button}
+        "mas" {Start-Process ("https://github.com/massgravel/Microsoft-Activation-Scripts") $Button}
+        "idm" { Start-Process ("https://github.com/WindowsAddict/IDM-Activation-Script") $Button}
+        "ittlink" { Start-Process ("https://github.com/emadadel4/ITT") $Button}
+        "eprojectslink" { Start-Process ("https://eprojects.orgfree.com/") $Button}
+        "teleegramprofile" {Start-Process ("https://t.me/emadadel4") $Button}
+        "window" { Write-Host "Bye see you soon. :)" StopAllRunspace StopMusic $Button
+        
+        }
+
     }
 }
