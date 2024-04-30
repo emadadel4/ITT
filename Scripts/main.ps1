85a390cb0dd7fa6f944872d7b0a9d05220d03631
Emad Adel <emadadel1920@gmail.com>
2024-04-30 22:37:20 +0300

    Update main.ps1

--- Scripts/main.ps1
+++ Scripts/main.ps1
@@ -15,16 +15,4 @@ else
 }
 #endregion
 
-
-#region Computer Managment tools
-$sync['window'].FindName('deviceManager').add_click({Start-Process devmgmt.msc})
-$sync['window'].FindName('services').add_click({Start-Process services.msc})
-$sync['window'].FindName('network').add_click({Start-Process ncpa.cpl})
-$sync['window'].FindName('sysinfo').add_click({Start-Process msinfo32.exe; dxdiag.exe})
-$sync['window'].FindName('poweroption').add_click({Start-Process powercfg.cpl})
-$sync['window'].FindName('appsfeatures').add_click({start-Process ms-settings:appsfeatures})
-$sync['window'].FindName('taskmgr').add_click({Start-Process taskmgr.exe})
-$sync['window'].FindName('diskmgmt').add_click({Start-Process diskmgmt.msc})
-#endregion Computer Managment tools
-
 $sync["window"].ShowDialog() | out-null
