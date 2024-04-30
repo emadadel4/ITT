a950c3799836c110d970c3e1f4b84065661490c1
Emad Adel <emadadel1920@gmail.com>
2024-05-01 02:33:04 +0300

    Update About-Dev.ps1

--- Functions/About-Dev.ps1
+++ Functions/About-Dev.ps1
@@ -4,6 +4,10 @@ function About{
     [xml]$ee = $childXaml
     $childWindowReader = (New-Object System.Xml.XmlNodeReader $ee)
     $childWindow = [Windows.Markup.XamlReader]::Load( $childWindowReader )
+
+
+    $childWindow.FindName('ver').Text = $sync.version
+
     $childWindow.ShowDialog() | Out-Null
 
 }
