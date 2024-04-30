90949fb3fc145167da27c34311bcb77d5b7d6f71
Emad Adel <emadadel1920@gmail.com>
2024-05-01 02:18:37 +0300

    Update main.ps1

--- Scripts/main.ps1
+++ Scripts/main.ps1
@@ -3,16 +3,7 @@ CheckChoco
 GetQuotes *> $null
 PlayMusic *> $null
 
-#region check currnet Theme
-if ($sync.theme -eq "Dark") {
-  Switch-ToDarkMode
-  $sync.isDarkMode = "Dark"
-} 
-else 
-{
-  Switch-ToLightMode
-  $sync.isDarkMode = "Light"
-}
-#endregion
+
+
 
 $sync["window"].ShowDialog() | out-null
