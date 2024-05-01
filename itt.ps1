a3879754d340ce5a8c30dfc983e01492a2b8d4bc
Emad Adel <emadadel1920@gmail.com>
2024-05-02 00:31:34 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -781,6 +781,7 @@ else
     $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
     $newProcess.Arguments = $myInvocation.MyCommand.Definition;
     $newProcess.Verb = "runas";
+    $newProcess.WindowStyle = "Maximized"
     [System.Diagnostics.Process]::Start($newProcess);
     break
 }
