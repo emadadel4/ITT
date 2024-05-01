0d6cbb740b6072358159c7fe478551684b0cb29c
Emad Adel <emadadel1920@gmail.com>
2024-05-02 01:50:16 +0300

    Add about links.

--- Functions/About-Dev.ps1
+++ Functions/About-Dev.ps1
@@ -4,10 +4,11 @@ function About{
     [xml]$ee = $childXaml
     $childWindowReader = (New-Object System.Xml.XmlNodeReader $ee)
     $childWindow = [Windows.Markup.XamlReader]::Load( $childWindowReader )
-
-
-    $childWindow.FindName('ver').Text = $sync.version
-
+    $childWindow.FindName('ver').Text = "Last update " + $sync.version
+    $childWindow.FindName("telegram").add_MouseLeftButtonDown({Start-Process("https://t.me/emadadel4")})
+    $childWindow.FindName("github").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4")})
+    $childWindow.FindName("website").add_MouseLeftButtonDown({Start-Process("https://eprojects.orgfree.com/")})
+    $childWindow.FindName("sourcecode").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4/ITT")})
     $childWindow.ShowDialog() | Out-Null
 
 }
