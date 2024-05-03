0596047cacd5041d3abd72636db0b49460033b83
Emad Adel <emadadel1920@gmail.com>
2024-05-03 20:05:31 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -93,45 +93,42 @@ function ClearFilter {
 function CheckChoco 
 {
     # Check if Chocolatey is installed
-    if (-not (Test-Path 'C:\ProgramData\chocolatey\choco.exe')) {
-
-Write-Host "
-+--------------------------------------------------------------------------------+
-| __        _______ _     ____ ___  __  __ _____   _____ ___    ___ _____ _____  |
-| \ \      / / ____| |   / ___/ _ \|  \/  | ____| |_   _/ _ \  |_ _|_   _|_   _| |
-|  \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|     | || | | |  | |  | |   | |   |
-|   \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| |  | |  | |   | |   |
-|    \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/  |___| |_|   |_|   |
-+--------------------------------------------------------------------------------+
-Starting up... It'll be quick. I hope you like the tool :)
+    if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
         
-" -ForegroundColor Red
-
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) *> $null
-
-    }
-    else
-    {
-
-Write-Host "
-+----------------------------------------------------------------------------+
-|  ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _      |
-| |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |     |
-|  | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |     |
-|  | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___  |
-| |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____| |
-|                                                                            |
-+----------------------------------------------------------------------------+
-Everything work fine. You good to go
-
-" -ForegroundColor green
-        
     }
 
     if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
 
-        iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+        Write-Host "
+        +--------------------------------------------------------------------------------+
+        | __        _______ _     ____ ___  __  __ _____   _____ ___    ___ _____ _____  |
+        | \ \      / / ____| |   / ___/ _ \|  \/  | ____| |_   _/ _ \  |_ _|_   _|_   _| |
+        |  \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|     | || | | |  | |  | |   | |   |
+        |   \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| |  | |  | |   | |   |
+        |    \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/  |___| |_|   |_|   |
+        +--------------------------------------------------------------------------------+
+        Starting up... It'll be quick. I hope you like the tool :)
+                
+        " -ForegroundColor Red
+        iex "& {$(irm get.scoop.sh)} -RunAsAdmin" | Out-Null
         scoop bucket add extras
+    }
+    else 
+    {
+
+        Write-Host "
+        +----------------------------------------------------------------------------+
+        |  ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _      |
+        | |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |     |
+        |  | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |     |
+        |  | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___  |
+        | |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____| |
+        |                                                                            |
+        +----------------------------------------------------------------------------+
+        Everything work fine. You good to go
+
+        " -ForegroundColor green
     }
 }
 
