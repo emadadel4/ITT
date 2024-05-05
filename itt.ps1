80859ca3f2e5dcf82834f51868e4f5f1ee8ed58a
Emad Adel <emadadel1920@gmail.com>
2024-05-05 23:55:58 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -83,54 +83,61 @@ function ClearFilter {
 }
 #endregion
 
-function CheckChoco 
-{
-    # Check if Chocolatey is installed
-    if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+function WriteAText {
+    param (
+        $message,
+        $color
+    )
+    
+$output = Write-Host "
+___ _____ _____   _____ __  __    _    ____    _    ____  _____ _     
+|_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \  / \  |  _ \| ____| |    
+ | |  | |   | |   |  _| | |\/| | / _ \ | | | |/ _ \ | | | |  _| | |    
+ | |  | |   | |   | |___| |  | |/ ___ \| |_| / ___ \| |_| | |___| |___ 
+|___| |_|   |_|   |_____|_|  |_/_/   \_\____/_/   \_\____/|_____|_____|
 
-Write-Host "
-+--------------------------------------------------------------------------------+
-| __        _______ _     ____ ___  __  __ _____   _____ ___    ___ _____ _____  |
-| \ \      / / ____| |   / ___/ _ \|  \/  | ____| |_   _/ _ \  |_ _|_   _|_   _| |
-|  \ \ /\ / /|  _| | |  | |  | | | | |\/| |  _|     | || | | |  | |  | |   | |   |
-|   \ V  V / | |___| |__| |__| |_| | |  | | |___    | || |_| |  | |  | |   | |   |
-|    \_/\_/  |_____|_____\____\___/|_|  |_|_____|   |_| \___/  |___| |_|   |_|   |
-+--------------------------------------------------------------------------------+
-Starting up... it won't take longer :)
+$message
 
 (IT Tools) is open source, You can contribute to improving the tool.
 If you have trouble installing a program, report the problem on feedback links
 https://github.com/emadadel4/ITT/issues
 https://t.me/emadadel4
 
-Nothing true everything permitted. 
-Freedom is not given it is taken.
-" -ForegroundColor White
+" -ForegroundColor "$color"
 
-        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) *> $null
+return $output
+
+}
+
+function WriteText {
+
+    param ([bool]$firstBoot)
+
+    if($firstBoot -eq $true)
+    {
+        Write-Host (WriteAText -color White -message  "Starting up... it won't take longer.") 
     }
     else
     {
-
-Write-Host "
-+----------------------------------------------------------------------------+
-|  ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _      |
-| |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |     |
-|  | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |     |
-|  | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___  |
-| |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____| |
-|                                                                            |
-+----------------------------------------------------------------------------+
-Everything work fine. You good to go
-
-(IT Tools) is open source, You can contribute to improving the tool.
-If you have trouble installing a program, report the problem on feedback links
-https://github.com/emadadel4/ITT/issues
-https://t.me/emadadel4
-" -ForegroundColor White
-
+        Write-Host (WriteAText -color White -message  "You ready to Install anything.") 
     }
+}
 
+
+function CheckChoco 
+{
+    # Check if Chocolatey is installed
+    if (-not (Get-Command choco -ErrorAction SilentlyContinue))
+    {
+        WriteText -firstBoot $true
+        Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) *> $null
+        Clear-Host
+        Write-Host (WriteAText -color White -message  "You ready to Install anything.") 
+    }
+    else
+    {
+        WriteText -firstBoot $false
+    }
 }
 
 
