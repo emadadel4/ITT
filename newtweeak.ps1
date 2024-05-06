ed71fa0ddfbb3e49f12a7ed2f5c80e077f3eb89f
Emad Adel <emadadel1920@gmail.com>
2024-05-06 23:49:15 +0300

    Generate a new Tweeak into /database/tweeaks.json file

---
+++ newtweeak.ps1
@@ -0,0 +1,39 @@
+# Prompt user for software information
+try {
+    Clear-Host
+    $Name = Read-Host "Enter app name"
+    $Description = (Read-Host "Enter app description").Trim()
+    $repo = (Read-Host "Enter repo url").Trim()  # Remove leading and trailing spaces
+    if ($repo -eq "") { $repo = "null" }  # Set default value if empty
+    $script = (Read-Host "Enter script").Trim()  # Remove leading and trailing spaces
+    if ($script -eq "") { $script = "none" }  # Set default value if empty
+    $check = "false" # default value is false
+
+    # Remove "choco install" from $choco if it exists
+    $choco = ($choco -replace "choco install", "" -replace ",,", ",").Trim()
+
+    # Define software object with sorted properties
+    $software = @{
+        Name = $Name
+        Description = $Description
+        repo = $repo
+        script = $script
+        check = $check
+    } | Select-Object Name, Description, repo, script, check
+
+    # Read existing JSON file
+    $jsonFilePath = "./Database/tweaks.json"
+    $existingData = Get-Content $jsonFilePath -ErrorAction Stop | ConvertFrom-Json
+
+    # Add new software object to existing array
+    $existingData += $software
+
+    # Write updated JSON to file
+    $existingData | ConvertTo-Json | Out-File $jsonFilePath -ErrorAction Stop
+
+    Write-Host  "Added Successfully" -ForegroundColor Green
+
+}
+catch {
+    Write-Host "An error occurred: $_"
+}
