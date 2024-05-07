faba2ae2fabb08334964c93d1a2da6879b50dbfb
Emad Adel <167635892+Wh0ami010@users.noreply.github.com>
2024-05-07 23:59:53 +0300

    Update Install-Choco.ps1

--- Scripts/Functions/Install-Choco.ps1
+++ Scripts/Functions/Install-Choco.ps1
@@ -42,8 +42,8 @@ function CheckChoco
     {
         WriteText -firstBoot $true
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) *> $null
-        Clear-Host
-        Write-Host (WriteAText -color White -message  "You ready to Install anything.") 
+        #Clear-Host
+        #Write-Host (WriteAText -color White -message  "You ready to Install anything.") 
     }
     else
     {
