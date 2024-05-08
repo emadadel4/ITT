c300964bba4aebd76e194d7e6bcdf34633603e36
Emad Adel <167635892+Wh0ami010@users.noreply.github.com>
2024-05-08 04:55:49 +0300

    +Useful extensions

--- Scripts/Functions/Invoke-Button.ps1
+++ Scripts/Functions/Invoke-Button.ps1
@@ -10,12 +10,15 @@ function Invoke-Button {
         "installBtn" {Invoke-Install $debug}
         "applyBtn" {Invoke-ApplyTweaks $debug}
         "taps" {ChangeTap $debug}
+        "category" {FilterByCat($sync.category.SelectedItem.Content) $debug}
+        # --Menu items-------------------
         "load" {LoadJson $Button}
         "save" {SaveItemsToJson $debug}
         "about" {About $debug}
-        "category" {FilterByCat($sync.category.SelectedItem.Content) $debug}
         "mas" {Start-Process ("https://github.com/massgravel/Microsoft-Activation-Scripts") $debug}
         "idm" { Start-Process ("https://github.com/WindowsAddict/IDM-Activation-Script") $debug}
+        "unhook" { Start-Process ("https://unhook.app/") $debug}
+        "uBlock" { Start-Process ("https://ublockorigin.com/") $debug}
         "dev" { About $Button}
         "deviceManager" {Start-Process devmgmt.msc $debug}
         "appsfeatures" {Start-Process ms-settings:appsfeatures $debug}
@@ -27,6 +30,7 @@ function Invoke-Button {
         "diskmgmt" {Start-Process diskmgmt.msc $debug}
         "darkOn" { Switch-ToDarkMode $debug }
         "darkOff" { Switch-ToLightMode $debug }
+        # --Menu items-------------------
         "searchInput" {Search; $sync['window'].FindName('category').SelectedIndex = 0; $sync['window'].FindName('apps').IsSelected = $true; $debug }
     }
 }
