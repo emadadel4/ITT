26efd91e8230fda2bacd1cb487b626cdfbe1ad77
Wh0ami010 <167635892+Wh0ami010@users.noreply.github.com>
2024-05-11 20:52:09 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -933,7 +933,6 @@ $sync.database = @{}
 $sync.ProcessRunning = $false
 $sync.isDarkMode
 $sync.mediaPlayer = New-Object -ComObject WMPlayer.OCX
-$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
 $currentPid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = new-object System.Security.Principal.WindowsPrincipal($currentPid)
 $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
@@ -945,6 +944,7 @@ if ($principal.IsInRole($adminRole))
 }
 else
 {
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
     $newProcess.Arguments = $myInvocation.MyCommand.Definition;
     $newProcess.Verb = "runas";
     [System.Diagnostics.Process]::Start($newProcess);
