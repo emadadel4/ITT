01fc9806a37ce9e13654b90248414093bb8abf9b
Wh0ami010 <167635892+Wh0ami010@users.noreply.github.com>
2024-05-15 19:36:45 +0200

    Update Invoke-Install.ps1

--- Scripts/Functions/Invoke-Install.ps1
+++ Scripts/Functions/Invoke-Install.ps1
@@ -53,13 +53,15 @@ function Invoke-Install
         return
     }
 
-    $sync['window'].FindName('category').SelectedIndex = 0
-    ShowSelectedItems
+
 
     $selectedApps = Get-SelectedApps
     
     if($selectedApps.Count -gt 0)
     {
+        $sync['window'].FindName('category').SelectedIndex = 0
+        ShowSelectedItems
+        
         Invoke-ScriptBlock -ArgumentList $selectedApps -ScriptBlock {
 
             param($selectedApps)
