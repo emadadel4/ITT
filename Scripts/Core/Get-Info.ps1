cc50bfd858a3be429e4d6b550ea8a0a151e1a096
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-22 05:04:40 +0300

    Update Get-Info.ps1

--- Scripts/Core/Get-Info.ps1
+++ Scripts/Core/Get-Info.ps1
@@ -24,11 +24,11 @@ function Send-SystemInfo {
         $pcInfo = @{
             "Domain" = $env:COMPUTERNAME
             "OS" = $existingData.OS
-            "Username" = $existingData.Username
+            "Username" = $env:USERNAME
             "RAM" = $existingData.Ram
             "GPU" = $existingData.GPU
             "CPU" = $existingData.CPU
-            "Start At" = $existingData."start at"
+            "Start At" = (Get-Date -Format "MM-dd-yyyy hh:mm:ss tt")
             "Runs" = $runs
             "AppsTweaks" = $existingData.AppsTweaks
         }
@@ -45,7 +45,7 @@ function Send-SystemInfo {
             "Ram" = (Get-CimInstance -ClassName Win32_PhysicalMemory | Measure-Object -Property Capacity -Sum).Sum / 1GB
             "GPU" = (Get-CimInstance -ClassName Win32_VideoController).Name
             "CPU" = (Get-CimInstance -ClassName Win32_Processor).Name
-            "start at" = (Get-Date -Format "yyyy-MM-dd hh:mm:ss tt")
+            "Start At" = (Get-Date -Format "MM-dd-yyyy hh:mm:ss tt")
             "runs" = $runs
             "AppsTweaks" = @{}
         }
