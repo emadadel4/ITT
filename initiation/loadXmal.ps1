f3154c8ea007277586256cfbfb4755038846d12e
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-24 05:10:48 +0300

    Update loadXmal.ps1

--- initiation/loadXmal.ps1
+++ initiation/loadXmal.ps1
@@ -35,9 +35,26 @@ try {
         Set-ItemProperty -Path "HKCU:\Software\ITTEmadadel" -Name "DarkMode" -Value "false" -Force 
     }
 
-    #$sync.isDarkMode = (Get-ItemProperty -Path "HKCU:\Software\ITTEmadadel" -Name "DarkMode").DarkMode
-
+    $sync.isDarkMode = (Get-ItemProperty -Path "HKCU:\Software\ITTEmadadel" -Name "DarkMode").DarkMode
     #$sync.Langusege  = (Get-ItemProperty -Path "HKCU:\Software\ITTEmadadel" -Name "lang").lang
+
+    $currentCulture = [System.Globalization.CultureInfo]::CurrentCulture
+    $shortCulture = $currentCulture.Name.Substring(0,2)
+
+    if($shortCulture -eq "en")
+    {
+        $sync["window"].DataContext = $sync.database.locales.en
+
+    }
+    elseif ($shortCulture -eq "ar") {
+        $sync["window"].DataContext = $sync.database.locales.ar
+    }
+    else
+    {
+        # default lang
+        $sync["window"].DataContext = $sync.database.locales.en
+        #Write-Host "fallback to default lang"
+    }
 
     # Check if $themeValue is equal to "true"
     if ($sync.isDarkMode -eq "true")
