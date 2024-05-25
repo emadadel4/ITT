d0b0ebd0220fa7885404f22ae77574b91dcd8e4b
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-25 13:55:57 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -8100,6 +8100,8 @@ function Get-SelectedApps
                                                 Scoop = $program.Scoop
                                                 Winget = $program.winget
                                                 Default = $program.default
+
+                                                # add a new method downloader here
                                             }
 
                                         }
@@ -8156,7 +8158,7 @@ function Invoke-Install
     
     if($sync.ProcessRunning)
     {
-        $msg = "Please wait for the software to be installed."
+        $msg = "Please wait there is a process in the background."
         [System.Windows.MessageBox]::Show($msg, "ITT", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
         return
     }
