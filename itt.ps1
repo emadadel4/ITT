26e1589963fbb89a259ecf8c670fdcb014167b1c
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-28 07:01:15 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -8731,8 +8731,45 @@ https://t.me/emadadel4
                     # Define the path to download the installer
                     $installerPath = "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 
-                    # Download the installer
-                    Invoke-WebRequest -Uri $url -OutFile $installerPath
+                    # Initialize the web request
+                    $webRequest = [System.Net.HttpWebRequest]::Create($url)
+                    $webRequest.Method = "GET"
+
+                    # Get the response
+                    $response = $webRequest.GetResponse()
+
+                    # Get the total size of the file
+                    $totalSize = $response.ContentLength
+
+                    # Open the response stream
+                    $responseStream = $response.GetResponseStream()
+
+                    # Create a file stream to write the downloaded data
+                    $fileStream = [System.IO.File]::Create($installerPath)
+
+                    # Buffer size for reading data
+                    $bufferSize = 8192
+                    $buffer = New-Object byte[] $bufferSize
+
+                    # Variables to track progress
+                    $totalRead = 0
+                    $readCount = 0
+
+                    # Read the data in chunks
+                    while (($readCount = $responseStream.Read($buffer, 0, $bufferSize)) -gt 0) {
+                        $fileStream.Write($buffer, 0, $readCount)
+                        $totalRead += $readCount
+
+                        # Calculate the percentage and display progress
+                        $percentComplete = [math]::Round(($totalRead / $totalSize) * 100, 2)
+                        $downloadedMB = [math]::Round($totalRead / 1MB, 2)
+                        $totalSizeMB = [math]::Round($totalSize / 1MB, 2)
+                        Write-Progress -Activity "Downloading winget" -Status "$downloadedMB MB of $totalSizeMB MB" -PercentComplete $percentComplete
+                    }
+
+                    # Close the streams
+                    $fileStream.Close()
+                    $responseStream.Close()
 
                     # Add-AppxPackage requires running with administrative privileges
                     Start-Process powershell -ArgumentList "Add-AppxPackage -Path $installerPath" -Verb RunAs -Wait
@@ -8749,6 +8786,7 @@ https://t.me/emadadel4
                 } else {
                     Write-Output "winget is already installed."
                 }
+
             }
 
             function SendApps {
