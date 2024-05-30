d4114215036565af03b3719e1d377f74606c98d5
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-30 05:59:38 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -8982,12 +8982,6 @@ https://t.me/emadadel4
             }
 
             function Get-WinUtilWingetLatest {
-                <#
-                .SYNOPSIS
-                    Uses GitHub API to check for the latest release of Winget.
-                .DESCRIPTION
-                    This function grabs the latest version of Winget and returns the download path to Install-WinUtilWinget for installation.
-                #>
             
                 Try{
                     # Grabs the latest release of Winget from the Github API for the install process.
@@ -9007,58 +9001,7 @@ https://t.me/emadadel4
             
             function Install-WinUtilWinget {
             
-                $isWingetInstalled = Test-WinUtilPackageManager -winget
-            
-                Try {
-                    if ($isWingetInstalled -eq "installed") {
-                        Write-Host "`nWinget is already installed.`r" -ForegroundColor Green
-                        return
-                    } elseif ($isWingetInstalled -eq "outdated") {
-                        Write-Host "`nWinget is Outdated. Continuing with install.`r" -ForegroundColor Yellow
-                    } else {
-                        Write-Host "`nWinget is not Installed. Continuing with install.`r" -ForegroundColor Red
-                    }
-            
-                    # Gets the computer's information
-                    if ($null -eq $sync.ComputerInfo){
-                        $ComputerInfo = Get-ComputerInfo -ErrorAction Stop
-                    } else {
-                        $ComputerInfo = $sync.ComputerInfo
-                    }
-            
-                    if (($ComputerInfo.WindowsVersion) -lt "1809") {
-                        # Checks if Windows Version is too old for Winget
-                        Write-Host "Winget is not supported on this version of Windows (Pre-1809)" -ForegroundColor Red
-                        return
-                    }
-            
-                    Write-Host "Downloading Winget Prerequsites`n"
-                    Get-WinUtilWingetPrerequisites
-                    Write-Host "Downloading Winget and License File`r"
-                    Get-WinUtilWingetLatest
-                    Write-Host "Installing Winget w/ Prerequsites`r"
-                    Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
-                    Write-Host "Manually adding Winget Sources, from Winget CDN."
-                    Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix #Seems some installs of Winget don't add the repo source, this should makes sure that it's installed every time. 
-                    Write-Host "Winget Installed" -ForegroundColor Green
-                    Write-Host "Enabling NuGet and Module..."
-                    Install-PackageProvider -Name NuGet -Force
-                    Install-Module -Name Microsoft.WinGet.Client -Force
-                    # Winget only needs a refresh of the environment variables to be used.
-                    Write-Output "Refreshing Environment Variables...`n"
-                    $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-                } Catch {
-                    Write-Host "Failure detected while installing via GitHub method. Continuing with Chocolatey method as fallback." -ForegroundColor Red
-                    # In case install fails via GitHub method.
-                    Try {
-                    Start-Process -Verb runas -FilePath powershell.exe -ArgumentList "choco install winget-cli"
-                    Write-Host "Winget Installed" -ForegroundColor Green
-                    Write-Output "Refreshing Environment Variables...`n"
-                    $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-                    } Catch {
-                        throw [WingetFailedInstall]::new('Failed to install!')
-                    }
-                }
+                Start-Process -FilePath "choco" -ArgumentList "install winget --confirm --acceptlicense -q -r --ignore-http-cache --allowemptychecksumsecure --allowemptychecksum --usepackagecodes --ignoredetectedreboot --ignore-checksums --ignore-reboot-requests" -NoNewWindow -Wait -PassThru
             }
 
             function Install-App {
