1ae655c6c55cc3ee36c89293082028f499218a32
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-30 06:18:18 +0300

    Update Invoke-Install.ps1

--- Scripts/Core/Invoke-Install.ps1
+++ Scripts/Core/Invoke-Install.ps1
@@ -339,8 +339,7 @@ https://t.me/emadadel4
             function Test-WinUtilPackageManager {
 
                 Param(
-                    [System.Management.Automation.SwitchParameter]$winget,
-                    [System.Management.Automation.SwitchParameter]$choco
+                    [System.Management.Automation.SwitchParameter]$winget
                 )
             
                 $status = "not-installed"
@@ -368,19 +367,17 @@ https://t.me/emadadel4
                         $response = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/Winget-cli/releases/latest" -Method Get -ErrorAction Stop
                         $wingetLatestVersion = [System.Version]::Parse(($response.tag_name).Trim('v')) #Stores version number of latest release.
                         $wingetOutdated = $wingetCurrentVersion -lt $wingetLatestVersion
-                        Write-Host "===========================================" -ForegroundColor Green
-                        Write-Host "---        Winget is installed          ---" -ForegroundColor Green
-                        Write-Host "===========================================" -ForegroundColor Green
-                        Write-Host "Version: $wingetVersionFull" -ForegroundColor White
+
+                        Write-Host "Winget is installed" -ForegroundColor Green
             
                         if (!$wingetPreview) {
-                            Write-Host "    - Winget is a release version." -ForegroundColor Green
+                            #Write-Host "    - Winget is a release version." -ForegroundColor Green
                         } else {
                             Write-Host "    - Winget is a preview version. Unexpected problems may occur." -ForegroundColor Yellow
                         }
             
                         if (!$wingetOutdated) {
-                            Write-Host "    - Winget is Up to Date" -ForegroundColor Green
+                            #Write-Host "    - Winget is Up to Date" -ForegroundColor Green
                             $status = "installed"
                         }
                         else {
@@ -394,22 +391,7 @@ https://t.me/emadadel4
                         $status = "not-installed"
                     }
                 }
-            
-                if ($choco) {
-                    if ((Get-Command -Name choco -ErrorAction Ignore) -and ($chocoVersion = (Get-Item "$env:ChocolateyInstall\choco.exe" -ErrorAction Ignore).VersionInfo.ProductVersion)) {
-                        Write-Host "===========================================" -ForegroundColor Green
-                        Write-Host "---      Chocolatey is installed        ---" -ForegroundColor Green
-                        Write-Host "===========================================" -ForegroundColor Green
-                        Write-Host "Version: v$chocoVersion" -ForegroundColor White
-                        $status = "installed"
-                    } else {
-                        Write-Host "===========================================" -ForegroundColor Red
-                        Write-Host "---    Chocolatey is not installed      ---" -ForegroundColor Red
-                        Write-Host "===========================================" -ForegroundColor Red
-                        $status = "not-installed"
-                    }
-                }
-            
+                
                 return $status
             }
             
@@ -417,12 +399,9 @@ https://t.me/emadadel4
             
                 $versionVCLibs = "14.00"
                 $fileVCLibs = "https://aka.ms/Microsoft.VCLibs.x64.${versionVCLibs}.Desktop.appx"
-                # Write-Host "$fileVCLibs"
-                # Microsoft.UI.Xaml version changed recently, so I made the version numbers variables.
                 $versionUIXamlMinor = "2.8"
                 $versionUIXamlPatch = "2.8.6"
                 $fileUIXaml = "https://github.com/microsoft/microsoft-ui-xaml/releases/download/v${versionUIXamlPatch}/Microsoft.UI.Xaml.${versionUIXamlMinor}.x64.appx"
-                # Write-Host "$fileUIXaml"
             
                 Try{
                     Write-Host "Downloading Microsoft.VCLibs Dependency..."
@@ -434,6 +413,24 @@ https://t.me/emadadel4
                     throw [WingetFailedInstall]::new('Failed to install prerequsites')
                 }
             }
+
+            function Get-WinUtilWingetLatest {
+            
+                Try{
+                    # Grabs the latest release of Winget from the Github API for the install process.
+                    $response = Invoke-RestMethod -Uri "https://api.github.com/repos/microsoft/Winget-cli/releases/latest" -Method Get -ErrorAction Stop
+                    $latestVersion = $response.tag_name #Stores version number of latest release.
+                    $licenseWingetUrl = $response.assets.browser_download_url[0] #Index value for License file.
+                    Write-Host "Latest Version:`t$($latestVersion)`n"
+                    $assetUrl = $response.assets.browser_download_url[2] #Index value for download URL.
+                    Invoke-WebRequest -Uri $licenseWingetUrl -OutFile $ENV:TEMP\License1.xml
+                    # The only pain is that the msixbundle for winget-cli is 246MB. In some situations this can take a bit, with slower connections.
+                    Invoke-WebRequest -Uri $assetUrl -OutFile $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle
+                }
+                Catch{
+                    throw [WingetFailedInstall]::new('Failed to get latest Winget release and license')
+                }
+            }
             
             function Install-WinUtilWinget {
             
@@ -441,12 +438,12 @@ https://t.me/emadadel4
             
                 Try {
                     if ($isWingetInstalled -eq "installed") {
-                        Write-Host "`nWinget is already installed.`r" -ForegroundColor Green
+                        #Write-Host "`nWinget is already installed.`r" -ForegroundColor Green
                         return
                     } elseif ($isWingetInstalled -eq "outdated") {
-                        Write-Host "`nWinget is Outdated. Continuing with install.`r" -ForegroundColor Yellow
+                        Write-Host "`nWinget is Outdated. updateing....`r" -ForegroundColor Yellow
                     } else {
-                        Write-Host "`nWinget is not Installed. Continuing with install.`r" -ForegroundColor Red
+                        Write-Host "`nWinget is not Installed. installing...`r" -ForegroundColor Red
                     }
             
                     # Gets the computer's information
@@ -471,10 +468,7 @@ https://t.me/emadadel4
                     Write-Host "Manually adding Winget Sources, from Winget CDN."
                     Add-AppxPackage -Path https://cdn.winget.microsoft.com/cache/source.msix #Seems some installs of Winget don't add the repo source, this should makes sure that it's installed every time. 
                     Write-Host "Winget Installed" -ForegroundColor Green
-                    Write-Host "Enabling NuGet and Module..."
-                    Install-PackageProvider -Name NuGet -Force
                     Install-Module -Name Microsoft.WinGet.Client -Force
-                    # Winget only needs a refresh of the environment variables to be used.
                     Write-Output "Refreshing Environment Variables...`n"
                     $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
                 } Catch {
