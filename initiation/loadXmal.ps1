f674386cb6407046c17d2e02f4eaecfeba0ba0c5
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-31 06:16:02 +0300

    Update loadXmal.ps1

--- initiation/loadXmal.ps1
+++ initiation/loadXmal.ps1
@@ -30,7 +30,7 @@ try {
     $currentCulture = [System.Globalization.CultureInfo]::CurrentCulture
     $shortCulture = $currentCulture.Name.Substring(0,2)
     
-    # Check if the registry key exists
+    # Check if the registry key exists if not then create one
     if (-not (Test-Path $sync.registryPath))
     {
         New-Item -Path "HKCU:\Software\itt.emadadel" -Force *> $null
@@ -41,6 +41,9 @@ try {
     $sync.isDarkMode = (Get-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "DarkMode").DarkMode
     $sync.Langusege  = (Get-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "locales").locales
 
+    #===========================================================================
+    #region Check for lang 
+    #===========================================================================
     if($sync.Langusege -ne "en")
     {
         switch ($sync.Langusege) {
@@ -62,8 +65,13 @@ try {
     {
         $sync["window"].DataContext = $sync.database.locales.$($sync.Langusege)
     }
+    #===========================================================================
+    #endregion Check for lang 
+    #===========================================================================
 
-    # Check if $themeValue is equal to "true"
+    #===========================================================================
+    #region Check Theme
+    #===========================================================================
     if ($sync.isDarkMode -eq "true")
     {
         $sync['window'].Resources.MergedDictionaries.Add($sync['window'].FindResource("Dark"))
@@ -71,6 +79,9 @@ try {
     } else {
         $sync['window'].Resources.MergedDictionaries.Add($sync['window'].FindResource("Light"))
     }
+    #===========================================================================
+    #endregion Check Theme
+    #===========================================================================
 
  }
 catch [System.Management.Automation.MethodInvocationException] {
