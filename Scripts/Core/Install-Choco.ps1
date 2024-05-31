bbbe3a8f874a7d9ed1a45874af4abcf42a3b2a13
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-05-31 04:23:32 +0300

    Update Install-Choco.ps1

--- Scripts/Core/Install-Choco.ps1
+++ Scripts/Core/Install-Choco.ps1
@@ -3,24 +3,23 @@ function WriteAText {
         $message,
         $color
     )
-    
-$output = Write-Host "
- ___ _____ _____   _____ __  __    _    ____    _    ____  _____ _     
- |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \  / \  |  _ \| ____| |    
-  | |  | |   | |   |  _| | |\/| | / _ \ | | | |/ _ \ | | | |  _| | |    
-  | |  | |   | |   | |___| |  | |/ ___ \| |_| / ___ \| |_| | |___| |___ 
-  |_|  |_|   |_|   |_____|_|  |_/_/   \_\____/_/   \_\____/|_____|_____|
 
- $message
- 
- (IT Tools) is open source, You can contribute to improving the tool.
- If you have trouble installing a program, report the problem on feedback links
- https://github.com/emadadel4/ITT/issues
- https://t.me/emadadel4
-" -ForegroundColor "$color"
-return $output
-
-
+Write-Host "+==============================================================================+";
+Write-Host "|                                                                              |";
+Write-Host "|                                                                              |";
+Write-Host "|   ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _       |";
+Write-Host "|  |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |      |";
+Write-Host "|   | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |      |";
+Write-Host "|   | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___   |";
+Write-Host "|  |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|  |";
+Write-Host "|                                                                              |";
+Write-Host "|                                                                              |";
+Write-Host "+==============================================================================+";
+Write-Host " `n` $message"
+Write-Host "`n` IT (Install and Tweaks Tools) is open source, You can contribute to improving the tool."
+Write-Host " If you have trouble installing a program, report the problem on feedback links"
+Write-Host " https://github.com/emadadel4/ITT/issues"
+Write-Host " https://t.me/emadadel4"
 }
 
 function Startup {
