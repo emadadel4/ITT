a93ddd2776fa92069017cd1886f7e351dff46757
Emad Adel <emadadel1920@gmail.com>
2024-06-05 10:07:55 +0300

    Update About-Dev.ps1

--- Scripts/UI/About-Dev.ps1
+++ Scripts/UI/About-Dev.ps1
@@ -1,25 +1,14 @@
 function About{
-
     # Load child window
     [xml]$about = $childXaml
-
     $sync.about = $childWindow
-
-
     $childWindowReader = (New-Object System.Xml.XmlNodeReader $about)
-
     $sync.about = [Windows.Markup.XamlReader]::Load( $childWindowReader )
-
     $sync.about.FindName('ver').Text = "Last update " + $sync.version
-
     $sync.about.FindName("telegram").add_MouseLeftButtonDown({Start-Process("https://t.me/emadadel4")})
-
     $sync.about.FindName("github").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4")})
-
     $sync.about.FindName("website").add_MouseLeftButtonDown({Start-Process("https://eprojects.orgfree.com/")})
-
     $sync.about.FindName("sourcecode").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4/ITT")})
-
 
     if($sync.Langusege -eq "en")
     {
@@ -31,8 +20,5 @@ function About{
         #$childWindow["window"].DataContext = $sync.database.locales.ar
         $sync.about.DataContext = $sync.database.locales.ar
     }
-
-    
     $sync.about.ShowDialog() | Out-Null
-
 }
