98fbfdaad531d3c76166d59628babb8049f5177a
Emad Adel <emadadel1920@gmail.com>
2024-06-06 04:18:25 +0300

    Update itt.ps1

--- Scripts/Core/Invoke-Install.ps1
+++ Scripts/Core/Invoke-Install.ps1
@@ -407,8 +407,9 @@ function Invoke-Install
                 # Check if Chocolatey is installed
                 if (-not (Get-Command choco -ErrorAction SilentlyContinue))
                 {
-                    Add-Log -Message "Installing Chocolatey for the first, It won't take minutes :)" -Level "WARNING"
+                    Add-Log -Message "Installing Chocolatey for the first time, It won't take minutes :)" -Level "INFO"
                     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) *> $null
+                    Add-Log -Message "Start Installing Selected apps." -Level "INFO"
                 }
             }
 
