19b2a3d4bdbadf1f7248dd75d253ce9114b60ecd
Emad Adel <emadadel1920@gmail.com>
2024-06-08 00:17:31 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -10003,36 +10003,6 @@ function GetQuotes {
     }
 }
 
-# Define OnClosing event handler
-$onClosingEvent = {
-    
-    param($s, $c)
-    
-    # Show confirmation message box
-    $result = [System.Windows.MessageBox]::Show("Are you sure you want to close the program, If there are any installing, this will end it", "Confirmation", [System.Windows.MessageBoxButton]::YesNo, [System.Windows.MessageBoxImage]::Question)
-
-    # Check user's choice
-    if ($result -eq "Yes")
-    {
-        # Close the window and stop all runspace
-        StopAllRunspace
-    }
-    else
-    {
-        # Cancel closing the window
-        $c.Cancel = $true
-    }
-}
-
-# Add OnClosing event handler to the window
-# Handle the Loaded event
-$sync["window"].Add_Loaded({
-    Startup
-    GetQuotes | Out-Null
-    PlayMusic | Out-Null
-    $sync["window"].Activate()
-})
-
 # Select all elements with a Name attribute using XPath and iterate over them
 $xaml.SelectNodes("//*[@Name]") | ForEach-Object {
     # Assign each element to a variable in $sync dictionary
@@ -10123,6 +10093,33 @@ $sync.Keys | ForEach-Object {
     }
 }
 
+# Define OnClosing event handler
+$onClosingEvent = {
+    
+    param($s, $c)
+    
+    # Show confirmation message box
+    $result = [System.Windows.MessageBox]::Show("Are you sure you want to close the program, If there are any installing, this will end it", "Confirmation", [System.Windows.MessageBoxButton]::YesNo, [System.Windows.MessageBoxImage]::Question)
+
+    # Check user's choice
+    if ($result -eq "Yes")
+    {
+        # Close the window and stop all runspace
+        StopAllRunspace
+    }
+    else
+    {
+        # Cancel closing the window
+        $c.Cancel = $true
+    }
+}
+
+# Handle the Loaded event
+$sync["window"].Add_Loaded({
+    GetQuotes | Out-Null
+    PlayMusic | Out-Null
+    $sync["window"].Activate()
+})
 
 #Close Event button
 $sync["window"].add_Closing($onClosingEvent)
