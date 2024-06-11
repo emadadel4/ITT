7dcd5206575882d28f34b2601b9113a4c365b273
Emad Adel <emadadel1920@gmail.com>
2024-06-11 06:59:57 +0300

    Update Local.ps1

--- Scripts/UI/Local.ps1
+++ Scripts/UI/Local.ps1
@@ -1,8 +1,11 @@
 function SetLangusege {
     param (
-        [string]$lang
+        [string]$lang  # Parameter for the language to set
     )
 
+    # Set DataContext of the window to the specified language
     $sync["window"].DataContext = $sync.database.locales.$($lang)
-    Set-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "locales" -Value "$lang" -Force 
+
+    # Set registry value for the language
+    Set-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "locales" -Value "$lang" -Force
 }
