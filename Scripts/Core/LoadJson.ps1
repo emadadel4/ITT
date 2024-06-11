94b15daba89e1fea9a55dc97e4dd2893735a4008
Emad Adel <emadadel1920@gmail.com>
2024-06-11 07:02:19 +0300

    Create LoadJson.ps1

---
+++ Scripts/Core/LoadJson.ps1
@@ -0,0 +1,123 @@
+
+function GetCheckBoxesFromStackPanel {
+    param (
+        [System.Windows.Controls.StackPanel]$item
+    )
+
+    $checkBoxes = @()  # Initialize an empty array to store CheckBoxes
+    
+    if ($item -is [System.Windows.Controls.StackPanel]) {
+        foreach ($child in $item.Children) {
+            if ($child -is [System.Windows.Controls.StackPanel]) {
+                foreach ($innerChild in $child.Children) {
+                    if ($innerChild -is [System.Windows.Controls.CheckBox]) {
+                        # Add CheckBox to the array
+                        $checkBoxes += $innerChild
+                    }
+                }
+            }
+        }
+    }
+    return $checkBoxes
+}
+
+function LoadJson {
+    if($sync.ProcessRunning)
+    {
+        $localizedMessageTemplate = $sync.database.locales.$($sync.Langusege).Pleasewait
+        $msg = "$localizedMessageTemplate"
+        [System.Windows.MessageBox]::Show($msg, "ITT", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
+        return
+    }
+    # Open file dialog to select JSON file
+    $openFileDialog = New-Object -TypeName "Microsoft.Win32.OpenFileDialog"
+    $openFileDialog.Filter = "JSON files (*.ea4)|*.ea4"
+    $openFileDialog.Title = "Open JSON File"
+    $dialogResult = $openFileDialog.ShowDialog()
+
+    if ($dialogResult -eq "OK") {
+
+        $jsonData = Get-Content -Path $openFileDialog.FileName -Raw | ConvertFrom-Json
+        $filteredNames = $jsonData
+
+        $filterPredicate = {
+
+            param($item)
+
+            $item =  GetCheckBoxesFromStackPanel -item $item
+
+            foreach ($currentItemName in $filteredNames.Name) {
+
+                if($currentItemName -eq $item.Content)
+                {
+                    $item.IsChecked = $true
+                    break
+                }
+
+            }
+            return $filteredNames.name -contains $item.Content
+        }
+        $sync['window'].FindName('apps').IsSelected = $true
+        $sync['window'].FindName('appslist').Clear()
+        $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync['window'].FindName('appslist').Items)
+        $collectionView.Filter = $filterPredicate
+        [System.Windows.MessageBox]::Show("Restored successfully", "ITT", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Information)
+    }
+}
+
+function SaveItemsToJson
+{
+    if($sync.ProcessRunning)
+    {
+        $localizedMessageTemplate = $sync.database.locales.$($sync.Langusege).Pleasewait
+        $msg = "$localizedMessageTemplate"
+        [System.Windows.MessageBox]::Show($msg, "ITT", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
+        return
+    }
+    $items = @()
+    ClearFilter
+    foreach ($item in $sync.AppsListView.Items)
+    {
+        $item =  GetCheckBoxesFromStackPanel -item $item
+        if ($item.IsChecked)
+        {
+                $itemObject = [PSCustomObject]@{
+                Name = $item.Content
+                check = "true"
+            }
+            $items += $itemObject
+        }
+    }
+
+    if ($null -ne $items -and $items.Count -gt 0) 
+    {
+        # Open save file dialog
+        $saveFileDialog = New-Object -TypeName "Microsoft.Win32.SaveFileDialog"
+        $saveFileDialog.Filter = "JSON files (*.ea4)|*.ea4"
+        $saveFileDialog.Title = "Save JSON File"
+        $dialogResult = $saveFileDialog.ShowDialog()
+
+        if ($dialogResult -eq "OK")
+        {
+            $items | ConvertTo-Json | Out-File -FilePath $saveFileDialog.FileName -Force
+            Write-Host "Saved: $($saveFileDialog.FileName)"
+
+            [System.Windows.MessageBox]::Show("Saved", "ITT", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Information)
+
+        }
+        
+            foreach ($item in $sync.AppsListView.Items)
+            {
+                $item =  GetCheckBoxesFromStackPanel -item $item
+
+                if ($item.IsChecked)
+                {
+                    $item.IsChecked = $false
+                }
+            }
+    }
+    else
+    {
+        [System.Windows.MessageBox]::Show("Choose at least one program", "ITT", [System.Windows.MessageBoxButton]::OK, [System.Windows.MessageBoxImage]::Warning)
+    }
+}
