32c07864fa69226874fdf6cf20d34e5d1fa7d18d
Emad Adel <emadadel1920@gmail.com>
2024-06-13 04:04:34 +0300

    Update newOST.ps1

--- newOST.ps1
+++ newOST.ps1
@@ -2,18 +2,14 @@ try {
 Clear-Host
 Write-Host "
 +-------------------------------------------------------------------------+
-|                                                                         |
 |    ___ _____ _____   ____    _  _____  _    ____    _    ____  _____    |
 |   |_ _|_   _|_   _| |  _ \  / \|_   _|/ \  | __ )  / \  / ___|| ____|   |
 |    | |  | |   | |   | | | |/ _ \ | | / _ \ |  _ \ / _ \ \___ \|  _|     |
 |    | |  | |   | |   | |_| / ___ \| |/ ___ \| |_) / ___ \ ___) | |___    |
 |   |___| |_|   |_|   |____/_/   \_\_/_/   \_\____/_/   \_\____/|_____|   |
-|                                                                         |
 |    Made with ♥  By Emad Adel                                            |
-|                                                                         |
 +-------------------------------------------------------------------------+
 "
-
     # Read existing JSON file
     $jsonFilePath = "./Assets/Database/OST.json"
     $existingData = Get-Content $jsonFilePath -Raw -ErrorAction Stop | ConvertFrom-Json
@@ -21,7 +17,7 @@ Write-Host "
     # Prompt for input
     $newOST = Read-Host "Enter the URL/ Example: www.eprojects.orgfree.com/ezio_family.mp3"
 
-    # store input
+    # Store input
     $track = $newOST
 
     # Add new object to existing array
