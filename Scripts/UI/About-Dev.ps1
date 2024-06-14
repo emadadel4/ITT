0b603854c1800a832e444ad68611a66678ccb9f9
Emad Adel <emadadel1920@gmail.com>
2024-06-14 23:45:37 +0300

    Update About-Dev.ps1

--- Scripts/UI/About-Dev.ps1
+++ Scripts/UI/About-Dev.ps1
@@ -5,7 +5,8 @@ function About{
     $childWindowReader = (New-Object System.Xml.XmlNodeReader $about)
     $sync.about = [Windows.Markup.XamlReader]::Load( $childWindowReader )
     $localizedMessageTemplate = $sync.database.locales.$($sync.Langusege).lastupdate
-    $sync.about.FindName('ver').Text = "$localizedMessageTemplate " + $sync.version
+    $sync.about.FindName('lastupdate').Text = $localizedMessageTemplate 
+    $sync.about.FindName('ver').Text =  $sync.version
     $sync.about.FindName("telegram").add_MouseLeftButtonDown({Start-Process("https://t.me/emadadel4")})
     $sync.about.FindName("github").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4")})
     $sync.about.FindName("website").add_MouseLeftButtonDown({Start-Process("https://eprojects.orgfree.com/")})
