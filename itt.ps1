f804cf30bebaec22375236a4f9dcdde123c58a1e
Emad Adel <emadadel1920@gmail.com>
2024-06-15 05:32:28 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -9717,67 +9717,58 @@ function Invoke-Button {
 
     Param ([string]$debug)
 
-    # debug
-    #Write-Host $debug
+    Switch -Wildcard ($debug) {
 
-    Switch -Wildcard ($debug){
+        "installBtn"        { Invoke-Install $debug }
+        "applyBtn"          { Invoke-ApplyTweaks $debug }
+        "taps"              { ChangeTap $debug }
+        "category"          { FilterByCat $sync.category.SelectedItem.Content $debug }
+        "searchInput"       { 
+                                Search
+                                $sync['window'].FindName('category').SelectedIndex = 0
+                                $debug 
+                             }
 
-        "installBtn" {Invoke-Install $debug}
-        "applyBtn" {Invoke-ApplyTweaks $debug}
-        "taps" {ChangeTap $debug}
-        "category" {FilterByCat($sync.category.SelectedItem.Content) $debug}
-        "searchInput" {Search; $sync['window'].FindName('category').SelectedIndex = 0; $debug }
+        # Menu items
 
-        #===========================================================================
-        #region Menu items
-        #===========================================================================
+        # Language
+        "ar"                { SetLangusege -lang "ar" $Button }
+        "en"                { SetLangusege -lang "en" $Button }
 
-        #region Lang
-        "ar" {SetLangusege -lang "ar" $Button}
-        "en" {SetLangusege -lang "en" $Button}
-        #endregion Lang 
+        "save"              { SaveItemsToJson $debug }
+        "load"              { LoadJson $Button }
 
-        "save" {SaveItemsToJson $debug}
-        "load" {LoadJson $Button}
+        # Device Management
+        "deviceManager"     {  devmgmt.msc $debug }
+        "appsfeatures"      {  appwiz.cpl $debug }
+        "sysinfo"           {  msinfo32.exe; Start-Process dxdiag.exe $debug }
+        "poweroption"       {  powercfg.cpl $debug }
+        "services"          {  services.msc $debug }
+        "network"           {  ncpa.cpl $debug }
+        "taskmgr"           {  taskmgr.exe $debug }
+        "diskmgmt"          {  diskmgmt.msc $debug }
 
-        #region Device Managment
-        "deviceManager" {Start-Process devmgmt.msc $debug}
-        "appsfeatures" {Start-Process ms-settings:appsfeatures $debug}
-        "sysinfo" {Start-Process msinfo32.exe; dxdiag.exe; $debug}
-        "poweroption" {Start-Process powercfg.cpl $debug}
-        "services" {Start-Process services.msc $debug}
-        "network" {Start-Process ncpa.cpl $debug}
-        "taskmgr" {Start-Process taskmgr.exe $debug}
-        "diskmgmt" {Start-Process diskmgmt.msc $debug}
-        #endregion Managment
+        # Theme
+        "Dark"              { Switch-ToDarkMode $debug }
+        "Light"             { Switch-ToLightMode $debug }
+        "systheme"          { SwitchToSystem $Button }
 
-        #region Theme
-            "Dark" { Switch-ToDarkMode $debug }
-            "Light" { Switch-ToLightMode $debug }
-            "systheme" {SwitchToSystem $Button}
-        #endregion Menu items
+        # Music
+        "moff"              { MuteMusic $debug }
+        "mon"               { Unmute $debug }
 
-        #region Music
-            "moff" { MuteMusic $debug }
-            "mon" { Unmute $debug }
-        #endregion Music
+        # Mirror Links
+        "unhook"            { Start-Process "https://unhook.app/" $debug }
+        "uBlock"            { Start-Process "https://ublockorigin.com/" $debug }
+        "mas"               { Start-Process "https://github.com/massgravel/Microsoft-Activation-Scripts" $debug }
+        "idm"               { Start-Process "https://github.com/WindowsAddict/IDM-Activation-Script" $debug }
+        "neat"              { Start-Process "https://addons.mozilla.org/en-US/firefox/addon/neatdownloadmanager-extension/" $debug }
 
-        #region Mirror Links
-        "unhook" { Start-Process ("https://unhook.app/") $debug}
-        "uBlock" { Start-Process ("https://ublockorigin.com/") $debug}
-        "mas" {Start-Process ("https://github.com/massgravel/Microsoft-Activation-Scripts") $debug}
-        "idm" { Start-Process ("https://github.com/WindowsAddict/IDM-Activation-Script") $debug}
-        "neat" { Start-Process ("https://addons.mozilla.org/en-US/firefox/addon/neatdownloadmanager-extension/")  $debug }
-        #endregion Mirror Links
-
-        "ittshortcut" { ITTShortcut $debug }
-        "dev" { About $Button}
-
-        #===========================================================================
-        #endregion Menu items
-        #===========================================================================
+        "ittshortcut"       { ITTShortcut $debug }
+        "dev"               { About $Button }
     }
 }
+
 function Invoke-Toogle {
 
     Param ([string]$debug)
