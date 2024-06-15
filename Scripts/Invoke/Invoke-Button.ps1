bcf91732193ca42234086f145b3fedc132e63e87
Emad Adel <emadadel1920@gmail.com>
2024-06-15 05:43:59 +0300

    Update Invoke-Button.ps1

--- Scripts/Invoke/Invoke-Button.ps1
+++ Scripts/Invoke/Invoke-Button.ps1
@@ -1,65 +1,146 @@
 function Invoke-Button {
+    Param ([string]$action)
 
-    Param ([string]$debug)
+    # Helper function for debugging
+    function Debug-Message($message) {
+        #Write-Host "Debug: $message"
+    }
 
-    # debug
-    #Write-Host $debug
+    # Switch block to handle different actions
+    Switch -Wildcard ($action) {
 
-    Switch -Wildcard ($debug){
+        "installBtn" {
+            Invoke-Install 
+            Debug-Message $action
+        }
+        "applyBtn" {
+            Invoke-ApplyTweaks
+            Debug-Message $action
+        }
+        "taps" {
+            ChangeTap $action
+        }
+        "category" {
+            FilterByCat($sync.category.SelectedItem.Content)
+            Debug-Message $action
+        }
+        "searchInput" {
+            Search
+            $sync['window'].FindName('category').SelectedIndex = 0
+            Debug-Message $action
+        }
 
-        "installBtn" {Invoke-Install $debug}
-        "applyBtn" {Invoke-ApplyTweaks $debug}
-        "taps" {ChangeTap $debug}
-        "category" {FilterByCat($sync.category.SelectedItem.Content) $debug}
-        "searchInput" {Search; $sync['window'].FindName('category').SelectedIndex = 0; $debug }
+        # Menu items
+        "ar" {
+            SetLangusege -lang "ar"
+            Debug-Message $action
+        }
+        "en" {
+            SetLangusege -lang "en"
+            Debug-Message $action
+        }
+        "save" {
+            SaveItemsToJson
+            Debug-Message $action
+        }
+        "load" {
+            LoadJson
+            Debug-Message $action
+        }
 
-        #===========================================================================
-        #region Menu items
-        #===========================================================================
+        # Device Management
+        "deviceManager" {
+            Start-Process devmgmt.msc 
+            Debug-Message $action
+        }
+        "appsfeatures" {
+            Start-Process appwiz.cpl 
+            Debug-Message $action
+        }
+        "sysinfo" {
+            Start-Process msinfo32.exe
+            Start-Process dxdiag.exe 
+            Debug-Message $action
+        }
+        "poweroption" {
+            Start-Process powercfg.cpl 
+            Debug-Message $action
+        }
+        "services" {
+            Start-Process services.msc 
+            Debug-Message $action
+        }
+        "network" {
+            Start-Process ncpa.cpl
+            Debug-Message $action
+        }
+        "taskmgr" {
+            Start-Process taskmgr.exe 
+            Debug-Message $action
+        }
+        "diskmgmt" {
+            Start-Process diskmgmt.msc
+            Debug-Message $action
+        }
 
-        #region Lang
-        "ar" {SetLangusege -lang "ar" $Button}
-        "en" {SetLangusege -lang "en" $Button}
-        #endregion Lang 
+        # Theme
+        "Dark" {
+            Switch-ToDarkMode
+            Debug-Message $action
+        }
+        "Light" {
+            Switch-ToLightMode
+            Debug-Message $action
+        }
+        "systheme" {
+            SwitchToSystem 
+            Debug-Message $action
+        }
 
-        "save" {SaveItemsToJson $debug}
-        "load" {LoadJson $Button}
+        # Music
+        "moff" {
+            MuteMusic
+            Debug-Message $action
+        }
+        "mon" {
+            UnmuteMusic
+            Debug-Message $action
+        }
 
-        #region Device Managment
-        "deviceManager" {Start-Process devmgmt.msc $debug}
-        "appsfeatures" {Start-Process ms-settings:appsfeatures $debug}
-        "sysinfo" {Start-Process msinfo32.exe; dxdiag.exe; $debug}
-        "poweroption" {Start-Process powercfg.cpl $debug}
-        "services" {Start-Process services.msc $debug}
-        "network" {Start-Process ncpa.cpl $debug}
-        "taskmgr" {Start-Process taskmgr.exe $debug}
-        "diskmgmt" {Start-Process diskmgmt.msc $debug}
-        #endregion Managment
+        # Mirror Links
+        "unhook" {
+            Start-Process "https://unhook.app/" 
+            Debug-Message $action
+        }
+        "uBlock" {
+            Start-Process "https://ublockorigin.com/" 
+            Debug-Message $action
+        }
+        "mas" {
+            Start-Process "https://github.com/massgravel/Microsoft-Activation-Scripts"
+            Debug-Message $action
+        }
+        "idm" {
+            Start-Process "https://github.com/WindowsAddict/IDM-Activation-Script"
+            Debug-Message $action
+        }
+        "neat" {
+            Start-Process "https://addons.mozilla.org/en-US/firefox/addon/neatdownloadmanager-extension/" 
+            Debug-Message $action
+        }
 
-        #region Theme
-            "Dark" { Switch-ToDarkMode $debug }
-            "Light" { Switch-ToLightMode $debug }
-            "systheme" {SwitchToSystem $Button}
-        #endregion Menu items
+        # Other actions
+        "ittshortcut" {
+            ITTShortcut $action
+            Debug-Message $action
+        }
+        "dev" {
+            About
+            Debug-Message $action
+        }
 
-        #region Music
-            "moff" { MuteMusic $debug }
-            "mon" { Unmute $debug }
-        #endregion Music
-
-        #region Mirror Links
-        "unhook" { Start-Process ("https://unhook.app/") $debug}
-        "uBlock" { Start-Process ("https://ublockorigin.com/") $debug}
-        "mas" {Start-Process ("https://github.com/massgravel/Microsoft-Activation-Scripts") $debug}
-        "idm" { Start-Process ("https://github.com/WindowsAddict/IDM-Activation-Script") $debug}
-        "neat" { Start-Process ("https://addons.mozilla.org/en-US/firefox/addon/neatdownloadmanager-extension/")  $debug }
-        #endregion Mirror Links
-
-        "ittshortcut" { ITTShortcut $debug }
-        "dev" { About $Button}
-
-        #===========================================================================
-        #endregion Menu items
-        #===========================================================================
+        Default {
+            Write-Host "Unknown action: $action"
+        }
     }
 }
