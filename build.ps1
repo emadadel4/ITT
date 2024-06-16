160932dfbf2351296690354355101ff1f1206714
Emad Adel <emadadel1920@gmail.com>
2024-06-16 20:05:39 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -83,9 +83,9 @@ function GenerateCheckboxes {
 
     $Checkboxes = ""
     
-    $ReversedItems = $Items[$Items.Count..0]
+    #$ReversedItems = $Items[$Items.Count..0]
 
-    foreach ($Item in $ReversedItems) {        
+    foreach ($Item in $Items) {        
 
         $CleanedItem = $Item.Description -replace '[^\w\s]', ''
 
