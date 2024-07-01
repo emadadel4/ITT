7b625cddaf919add7bbf3d435bd3c265c7e0becb
Wh0ami010 <nocomment.emad@gmail.com>
2024-07-01 19:54:54 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -8758,29 +8758,6 @@ function Invoke-ApplyTweaks {
                                 }
                             }
                         })
-    
-                        Start-Sleep 10
-    
-                        Clear-Host
-    
-                        Write-Host "+==============================================================================+";
-                        Write-Host "|                                                                              |";
-                        Write-Host "|                                                                              |";
-                        Write-Host "|   ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _       |";
-                        Write-Host "|  |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |      |";
-                        Write-Host "|   | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |      |";
-                        Write-Host "|   | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___   |";
-                        Write-Host "|  |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|  |";
-                        Write-Host "|                                                                              |";
-                        Write-Host "|                                                                              |";
-                        Write-Host "+==============================================================================+";
-                        Write-Host "`n` You ready to Install anything."
-                        Write-Host  "`n` (IT Tools) is open source, You can contribute to improving the tool."
-                        Write-Host " If you have trouble installing a program, report the problem on feedback links"
-                        Write-Host  " https://github.com/emadadel4/ITT/issues"
-                        Write-Host  " https://t.me/emadadel4"
-    
-             
                     }
                     
                     function CustomMsg 
