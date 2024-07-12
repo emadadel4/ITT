f4c05f546923370414815f733958b04170998bea
Emad Adel <emadadel1920@gmail.com>
2024-07-12 04:54:38 +0300

    Update xaml.ps1

--- initiation/xaml.ps1
+++ initiation/xaml.ps1
@@ -34,7 +34,7 @@ try {
     {
         New-Item -Path "HKCU:\Software\itt.emadadel" -Force *> $null
         Set-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "DarkMode" -Value "none" -Force 
-        Set-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "locales" -Value "$( $shortCulture)" -Force 
+        Set-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "locales" -Value "$($shortCulture)" -Force 
     }
 
     $sync.isDarkMode = (Get-ItemProperty -Path "HKCU:\Software\itt.emadadel" -Name "DarkMode").DarkMode
