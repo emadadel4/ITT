fb183a2802f8cd59b768b576ce0ece0d348c1677
Emad Adel <emadadel1920@gmail.com>
2024-07-18 03:59:52 +0300

    optimize

--- initiation/start.ps1
+++ initiation/start.ps1
@@ -1,36 +1,34 @@
 # Load DLLs
 Add-Type -AssemblyName System.Windows.Forms
 
-# Variable to sync between runspaces
-$sync = [Hashtable]::Synchronized(@{})
-$sync.database = @{}
-$sync.ProcessRunning = $false
-$sync.lastupdate = "#{replaceme}"
-$sync.github =   "https://github.com/emadadel4"
-$sync.telegram = "https://t.me/emadadel4"
-$sync.website =  "https://emadadel4.github.io"
-$sync.developer =   "Emad Adel"
-$sync.registryPath = "HKCU:\Software\itt.emadadel"
-$sync.firebaseUrl = "https://ittools-7d9fe-default-rtdb.firebaseio.com/Users"
-$sync.isDarkMode
-$sync.Langusege = "en"
-$sync.mediaPlayer = New-Object -ComObject WMPlayer.OCX
-$currentPid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = new-object System.Security.Principal.WindowsPrincipal($currentPid)
-$administrator=[System.Security.Principal.WindowsBuiltInRole]::Administrator
-$newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+# Synchronized Hashtable for shared variables
+$sync = [Hashtable]::Synchronized(@{
+    database       = @{}
+    ProcessRunning = $false
+    lastupdate     = "07/18/24"
+    github         = "https://github.com/emadadel4"
+    telegram       = "https://t.me/emadadel4"
+    website        = "https://emadadel4.github.io"
+    developer      = "Emad Adel"
+    registryPath   = "HKCU:\Software\itt.emadadel"
+    firebaseUrl    = "https://ittools-7d9fe-default-rtdb.firebaseio.com/Users"
+    isDarkMode     = $null
+    Langusege      = "en"
+    mediaPlayer    = New-Object -ComObject WMPlayer.OCX
+})
 
-if ($principal.IsInRole($administrator))
-{
-    $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Admin)"
-    $Host.UI.RawUI.WindowTitle = "ITT (Install and Tweaks Tool)"
-    #Clear-Host
-}
-else
-{
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-    $newProcess.Verb = "runas";
-    [System.Diagnostics.Process]::Start($newProcess);
+$currentPid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal($currentPid)
+$administrator = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+$newProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell"
+
+if ($principal.IsInRole($administrator)) {
+    $Host.UI.RawUI.WindowTitle = "ITT (Install and Tweaks Tool) - Admin"
+} else {
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition
+    $newProcess.Verb = "runas"
+    [System.Diagnostics.Process]::Start($newProcess)
     exit
-    break
 }
+
