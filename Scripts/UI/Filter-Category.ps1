9aa7746d442643091e462d6ace31f9b3e4e8c1dc
Emad Adel <emadadel1920@gmail.com>
2024-07-19 06:04:37 +0300

    Update Filter-Category.ps1

--- Scripts/UI/Filter-Category.ps1
+++ Scripts/UI/Filter-Category.ps1
@@ -24,14 +24,35 @@ function Search {
         return $true  # Non-StackPanel items are always included
     }
 }
+
 function FilterByCat {
 
     param ($Cat)
 
-    $sync["window"].DataContext = $sync.database.locales.Controls.$($sync.Langusege)
+    # Define the list of valid categories
+    $validCategories = @(
+        "Web Browsers",
+        "Media",
+        "Media Tools",
+        "Documents",
+        "Compression",
+        "Communication",
+        "File Sharing",
+        "Imaging",
+        "Gaming",
+        "Utilities",
+        "Disk Tools",
+        "Development",
+        "Security",
+        "Portable",
+        "Runtimes",
+        "Drivers"
+    )
 
+    # Update DataContext
+    #$sync["window"].DataContext = $sync.database.locales.Controls.$($sync.Langusege)
 
-    # if user on Other tab return to apps list
+    # if user is on another tab, return to the apps list
     $sync['window'].FindName('apps').IsSelected = $true
     $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync.AppsListView.Items)
 
@@ -40,18 +61,15 @@ function FilterByCat {
         param($item)
         
         if ($item -is [System.Windows.Controls.StackPanel]) {
-
             foreach ($child in $item.Children) {
                 if ($child -is [System.Windows.Controls.StackPanel]) {
                     foreach ($innerChild in $child.Children) {
                         if ($innerChild -is [System.Windows.Controls.CheckBox]) {
-
                             # Define the tag you want to filter by
                             $tagToFilter =  $Cat
                             # Check if the item has the tag
                             $itemTag = $innerChild.Tag
                             return $itemTag -eq $tagToFilter
-
                         }
                     }
                 }
@@ -59,21 +77,14 @@ function FilterByCat {
         }
     }
 
-    if($Cat -eq $sync.database.locales.Controls.$($sync.Langusege).all)
-    {
-        $sync.AppsListView.Clear()
-        $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync.AppsListView.Items)
-        $collectionView.Filter = $null
-    }
-    else
-    {
-        $sync.AppsListView.Clear()
+    if ($validCategories -contains $Cat) {
         # Apply the filter to the collection view
+        $sync.AppsListView.Clear()
         $collectionView.Filter = $filterPredicate
     }
-}
-function ClearFilter {
-    $sync.AppsListView.Clear()
-    $collectionView = [System.Windows.Data.CollectionViewSource]::GetDefaultView($sync.AppsListView.Items)
-    $collectionView.Filter = $null
+    else {
+        # Clear the filter if selected category is not in the predefined list
+        $sync.AppsListView.Clear()
+        $collectionView.Filter = $null
+    }
 }
