be604d78f0df0becb0fe59bd27efb333df5924ad
Wh0ami010 <nocomment.emad@gmail.com>
2024-07-20 20:52:16 +0300

    Change shourtcut icon

--- Scripts/UI/Create-Shortcut.ps1
+++ Scripts/UI/Create-Shortcut.ps1
@@ -2,8 +2,9 @@ function ITTShortcut {
     # URL of the icon file
     $iconUrl = "https://raw.githubusercontent.com/emadadel4/ITT/main/Assets/Icons/icon.ico"
     
-    # Local path where the icon will be downloaded
-    $localIconPath = "$env:TEMP\ITTIcon.ico"
+    # Determine the path in AppData\Roaming
+    $appDataPath = [Environment]::GetFolderPath('ApplicationData')
+    $localIconPath = Join-Path -Path $appDataPath -ChildPath "ITTIcon.ico"
     
     # Download the icon file
     Invoke-WebRequest -Uri $iconUrl -OutFile $localIconPath
@@ -15,7 +16,7 @@ function ITTShortcut {
     $Shortcut.TargetPath = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
     $Shortcut.Arguments = "-ExecutionPolicy Bypass -Command ""irm bit.ly/emadadel | iex"""
     
-    # Set the icon path to the downloaded icon file
+    # Set the icon path to the downloaded icon file in AppData\Roaming
     $Shortcut.IconLocation = "$localIconPath"
     
     # Save the shortcut
--- itt.ps1
+++ itt.ps1
@@ -11186,8 +11186,9 @@ function ITTShortcut {
     # URL of the icon file
     $iconUrl = "https://raw.githubusercontent.com/emadadel4/ITT/main/Assets/Icons/icon.ico"
     
-    # Local path where the icon will be downloaded
-    $localIconPath = "$env:TEMP\ITTIcon.ico"
+    # Determine the path in AppData\Roaming
+    $appDataPath = [Environment]::GetFolderPath('ApplicationData')
+    $localIconPath = Join-Path -Path $appDataPath -ChildPath "ITTIcon.ico"
     
     # Download the icon file
     Invoke-WebRequest -Uri $iconUrl -OutFile $localIconPath
@@ -11199,7 +11200,7 @@ function ITTShortcut {
     $Shortcut.TargetPath = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\powershell.exe"
     $Shortcut.Arguments = "-ExecutionPolicy Bypass -Command ""irm bit.ly/emadadel | iex"""
     
-    # Set the icon path to the downloaded icon file
+    # Set the icon path to the downloaded icon file in AppData\Roaming
     $Shortcut.IconLocation = "$localIconPath"
     
     # Save the shortcut
