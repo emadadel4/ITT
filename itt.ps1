6077e4e98ff6820af0e97af7c72da22d68dd671f
Emad Adel <emadadel1920@gmail.com>
2024-07-24 02:33:35 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -10301,7 +10301,7 @@ function Get-PCInfo {
                 }
             }
         
-            Write-Host -NoNewline "`n` $totalKeys" -ForegroundColor Red
+            Write-Host -NoNewline "`n`  $totalKeys" -ForegroundColor Red
             Write-Host " devices use this tool." -ForegroundColor Yellow
         
         } | Out-Null
