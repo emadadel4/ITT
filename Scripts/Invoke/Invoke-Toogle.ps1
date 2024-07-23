1791ad0bb3be3990c784b53fdb7eedce07942d20
Emad Adel <emadadel1920@gmail.com>
2024-07-23 05:20:31 +0300

    Update Invoke-Toogle.ps1

--- Scripts/Invoke/Invoke-Toogle.ps1
+++ Scripts/Invoke/Invoke-Toogle.ps1
@@ -9,7 +9,6 @@ function Invoke-Toogle {
 
         "ToggleShowExt" {Invoke-ShowFile-Extensions $(Get-ToggleStatus ToggleShowExt)}
         "ToggleDarkMode" {Invoke-DarkMode $(Get-ToggleStatus ToggleDarkMode)}
-
         "ToggleShowHidden" {Invoke-ShowFile $(Get-ToggleStatus ToggleShowHidden)}
     }
 }
