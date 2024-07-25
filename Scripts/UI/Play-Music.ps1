79373b0d3bcd91f2804f4e61d5a0194dd2ebb17b
Emad Adel <emadadel1920@gmail.com>
2024-07-25 04:55:16 +0300

    Update Play-Music.ps1

--- Scripts/UI/Play-Music.ps1
+++ Scripts/UI/Play-Music.ps1
@@ -6,15 +6,18 @@ function PlayMusic {
         $sync.mediaPlayer.controls.play()
     }
 
-    # Shuffle the playlist
-    $shuffledTracks = $sync.database.OST.Tracks | Get-Random -Count $sync.database.OST.Tracks.Count
+    # Shuffle the playlist and create a new playlist
+    function GetShuffledTracks {
+        return $sync.database.OST.Tracks | Get-Random -Count $sync.database.OST.Tracks.Count
+    }
 
     # Function to play the shuffled playlist
     function PlayShuffledPlaylist {
+        $shuffledTracks = GetShuffledTracks
         foreach ($url in $shuffledTracks) {
             PlayAudio $url
             # Wait for the track to finish playing
-            while ($sync.mediaPlayer.playState -eq 3 -or $sync.mediaPlayer.playState -eq 6) {
+            while ($sync.mediaPlayer.playState -in 3, 6) {
                 Start-Sleep -Milliseconds 100
             }
         }
@@ -23,7 +26,6 @@ function PlayMusic {
     # Play the shuffled playlist
     PlayShuffledPlaylist
 }
-
 function MuteMusic {
 
     $sync.mediaPlayer.settings.volume = 0
@@ -46,5 +48,5 @@ function StopAllRunspace {
     $script:powershell.Stop()
     StopMusic
     $newProcess.exit
-    Write-Host "Bye see you soon. :)" 
+    Write-Host "`n` Bye see you soon. :)" 
 }
