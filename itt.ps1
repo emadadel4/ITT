811f66c8129c273c00dbb781d908f6072a5dcebd
Emad Adel <emadadel1920@gmail.com>
2024-07-25 07:02:58 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -11408,29 +11408,22 @@ function Invoke-ShowFile-Extensions {
         Write-Warning $psitem.Exception.StackTrace
     }
 }
-function About{
+function About {
     # Load child window
     [xml]$about = $childXaml
-    $sync.about = $childWindow
     $childWindowReader = (New-Object System.Xml.XmlNodeReader $about)
-    $sync.about = [Windows.Markup.XamlReader]::Load( $childWindowReader )
-    $sync.about.FindName('ver').Text =  $sync.lastupdate
+    $sync.about = [Windows.Markup.XamlReader]::Load($childWindowReader)
+    # Set version and link handlers
+    $sync.about.FindName('ver').Text = $sync.lastupdate
     $sync.about.FindName("telegram").add_MouseLeftButtonDown({Start-Process("https://t.me/emadadel4")})
     $sync.about.FindName("github").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4")})
     $sync.about.FindName("website").add_MouseLeftButtonDown({Start-Process("https://emadadel4.github.io")})
     $sync.about.FindName("sourcecode").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4/ITT")})
     $sync.about.FindName("coffee").add_MouseLeftButtonDown({Start-Process("https://buymeacoffee.com/emadadel")})
-
-    if($sync.Langusege -eq "en")
-    {
-        #$childWindow["window"].DataContext = $sync.database.locales.en
-        $sync.about.DataContext = $sync.database.locales.Controls.en
-    }
-    else
-    {
-        #$childWindow["window"].DataContext = $sync.database.locales.ar
-        $sync.about.DataContext = $sync.database.locales.Controls.ar
-    }
+    # Set data context based on language
+    $locale = if ($sync.Langusege -eq "en") { "en" } else { "ar" }
+    $sync.about.DataContext = $sync.database.locales.Controls.$locale
+    # Show dialog
     $sync.about.ShowDialog() | Out-Null
 }
 function ITTShortcut {
