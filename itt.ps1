982ea41e38ff9655b33a7159981811b57daa751a
Emad Adel <emadadel1920@gmail.com>
2024-08-04 23:51:54 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -11999,27 +11999,27 @@ function WriteAText {
         $color
     )
     
-Write-Host " +==============================================================================+" -ForegroundColor Yellow;
-Write-Host " |   ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _       |" -ForegroundColor Yellow;
-Write-Host " |  |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |      |" -ForegroundColor Yellow;
-Write-Host " |   | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |      |" -ForegroundColor Yellow;
-Write-Host " |   | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___   |" -ForegroundColor Yellow;
-Write-Host " |  |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|  |" -ForegroundColor Yellow;
-Write-Host " |                       Made with ♥ By Emad Adel                               |" -ForegroundColor Yellow;
-Write-Host " |                          #StandWithPalestine                                 |" -ForegroundColor Yellow;
-Write-Host " +==============================================================================+" -ForegroundColor Yellow; 
-Write-Host " $message `n` " -ForegroundColor Yellow
-Write-Host " Github repo: https://github.com/emadadel4/ITT/issues" -ForegroundColor Yellow
-Write-Host " Telegram: https://t.me/ittemadadel" -ForegroundColor Yellow
-Write-Host " Discord: https://discord.com/invite/3eV79KgD" -ForegroundColor Yellow
-Write-Host " Commands:" -ForegroundColor Yellow
-Write-Host " irm bit.ly/ittea | iex" -ForegroundColor Yellow
-Write-Host " irm cutt.ly/ittea | iex" -ForegroundColor Yellow
-Write-Host " irm bit.ly/emadadel | iex" -ForegroundColor Yellow
+    Write-Host " +==============================================================================+" -ForegroundColor Yellow;
+    Write-Host " |   ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _       |" -ForegroundColor Yellow;
+    Write-Host " |  |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |      |" -ForegroundColor Yellow;
+    Write-Host " |   | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |      |" -ForegroundColor Yellow;
+    Write-Host " |   | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___   |" -ForegroundColor Yellow;
+    Write-Host " |  |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|  |" -ForegroundColor Yellow;
+    Write-Host " |                       Made with ♥ By Emad Adel                               |" -ForegroundColor Yellow;
+    Write-Host " |                          #StandWithPalestine                                 |" -ForegroundColor Yellow;
+    Write-Host " +==============================================================================+" -ForegroundColor Yellow; 
+    Write-Host " $message `n` " -ForegroundColor Yellow
+    Write-Host " Github repo: https://github.com/emadadel4/ITT/issues" -ForegroundColor Yellow
+    Write-Host " Telegram: https://t.me/ittemadadel" -ForegroundColor Yellow
+    Write-Host " Discord: https://discord.com/invite/3eV79KgD" -ForegroundColor Yellow
+    Write-Host " Commands:" -ForegroundColor Yellow
+    Write-Host " irm bit.ly/ittea | iex" -ForegroundColor Yellow
+    Write-Host " irm cutt.ly/ittea | iex" -ForegroundColor Yellow
+    Write-Host " irm bit.ly/emadadel | iex" -ForegroundColor Yellow
 }
 function Startup {
     Write-Host (WriteAText -color White -message  "You ready to Install anything.") 
-    Get-PCInfo 
+    Get-PCInfo  
 }
 function GetCheckBoxesFromStackPanel {
     param (
@@ -12143,27 +12143,25 @@ function SaveItemsToJson {
     }
 }
 function ChangeTap() {
-
-    if($sync['window'].FindName('apps').IsSelected)
-    {
-        $sync['window'].FindName('installBtn').Visibility = "Visible"
-        $sync['window'].FindName('applyBtn').Visibility = "Hidden"
-        $sync.currentList = "appslist"
+    # Define a hash table to map tab names to their button visibility and list values
+    $tabSettings = @{
+        'apps' = @{ 'installBtn' = 'Visible'; 'applyBtn' = 'Hidden'; 'currentList' = 'appslist' }
+        'tweeksTab' = @{ 'installBtn' = 'Hidden'; 'applyBtn' = 'Visible'; 'currentList' = 'tweakslist' }
+        'SettingsTab' = @{ 'installBtn' = 'Hidden'; 'applyBtn' = 'Hidden'; 'currentList' = $null }
     }
 
-    if($sync['window'].FindName('tweeksTab').IsSelected)
-    {
-        $sync['window'].FindName('applyBtn').Visibility = "Visible"
-        $sync['window'].FindName('installBtn').Visibility = "Hidden"
-        $sync.currentList = "tweakslist"
-    }
-
-    if($sync['window'].FindName('SettingsTab').IsSelected)
-    {
-        $sync['window'].FindName('applyBtn').Visibility = "Hidden"
-        $sync['window'].FindName('installBtn').Visibility = "Hidden"
+    # Iterate over the tabs and update visibility and currentList based on the selected tab
+    foreach ($tab in $tabSettings.Keys) {
+        if ($sync['window'].FindName($tab).IsSelected) {
+            $settings = $tabSettings[$tab]
+            $sync['window'].FindName('installBtn').Visibility = $settings['installBtn']
+            $sync['window'].FindName('applyBtn').Visibility = $settings['applyBtn']
+            $sync.currentList = $settings['currentList']
+            break
+        }
     }
 }
+
 function Invoke-Button {
     Param ([string]$action)
 
