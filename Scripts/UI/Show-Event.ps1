9fa123687d3821fa07627c5304bbf96740df4647
Emad Adel <emadadel1920@gmail.com>
2024-08-07 01:44:24 +0300

    Update Show-Event.ps1

--- Scripts/UI/Show-Event.ps1
+++ Scripts/UI/Show-Event.ps1
@@ -68,6 +68,6 @@ function Check-DateAndShowEvent {
     } 
     else 
     {
-        Show-Event -image "https://raw.githubusercontent.com/emadadel4/ITT/main/Assets/Images/thumbnail.png" -title "Watch tutorial" -day "Default" -WindowHeight 455 -WindowWidth 555
+        Show-Event -image "https://raw.githubusercontent.com/emadadel4/ITT/main/Assets/Images/thumbnail.png" -title "Watch demo" -day "Default" -WindowHeight 455 -WindowWidth 555
     }
 }
