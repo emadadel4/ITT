d04e3bcdd95d8210e783716a2848e09ecf5ff4b0
Emad Adel <emadadel1920@gmail.com>
2024-08-06 03:08:45 +0300

    Update Install-Apps.ps1

--- Scripts/Core/Install-Apps.ps1
+++ Scripts/Core/Install-Apps.ps1
@@ -226,7 +226,7 @@ function Invoke-Install {
                                         "CPU" = $existingData.CPU
                                         "Cores" = $existingData.Cores 
                                         "Language" = $existingData.Language 
-                                        "Start at" = ($sync.Date)
+                                        "Start at" = (Get-Date -Format "MM/dd/yyyy hh:mm:ss tt")
                                         "Runs" = $existingData.Runs
                                         "AppsHistory" = $selectedItemContent
                                         "TweaksHistory" = $existingData.TweaksHistory
