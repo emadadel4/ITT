3f4c116da61eec94ba6e45332090b111f6f096bf
Emad Adel <emadadel1920@gmail.com>
2024-08-13 01:55:25 +0300

    documentation and explanation

--- Scripts/Core/Tap-On-Selection-Changed.ps1
+++ Scripts/Core/Tap-On-Selection-Changed.ps1
@@ -1,19 +1,23 @@
-function ChangeTap() {
+function ChangeTab {
     # Define a hash table to map tab names to their button visibility and list values
     $tabSettings = @{
-        'apps' = @{ 'installBtn' = 'Visible'; 'applyBtn' = 'Hidden'; 'currentList' = 'appslist' }
-        'tweeksTab' = @{ 'installBtn' = 'Hidden'; 'applyBtn' = 'Visible'; 'currentList' = 'tweakslist' }
+        'apps'        = @{ 'installBtn' = 'Visible'; 'applyBtn' = 'Hidden'; 'currentList' = 'appslist' }
+        'tweeksTab'   = @{ 'installBtn' = 'Hidden'; 'applyBtn' = 'Visible'; 'currentList' = 'tweakslist' }
         'SettingsTab' = @{ 'installBtn' = 'Hidden'; 'applyBtn' = 'Hidden'; 'currentList' = $null }
     }
 
-    # Iterate over the tabs and update visibility and currentList based on the selected tab
+    # Iterate over the tab settings
     foreach ($tab in $tabSettings.Keys) {
+        # Check if the current tab is selected
         if ($sync['window'].FindName($tab).IsSelected) {
             $settings = $tabSettings[$tab]
+            
+            # Update button visibility and currentList based on the selected tab
             $sync['window'].FindName('installBtn').Visibility = $settings['installBtn']
             $sync['window'].FindName('applyBtn').Visibility = $settings['applyBtn']
             $sync.currentList = $settings['currentList']
-            break
+            
+            break  # Exit the loop once the matching tab is found
         }
     }
 }
