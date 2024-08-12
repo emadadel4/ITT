618587a9bcbff3e852c3bc2253e0e9667c996cee
Emad Adel <emadadel1920@gmail.com>
2024-08-13 01:52:39 +0300

    documentation and explanation

--- Scripts/Core/Get-ToggleStatus.ps1
+++ Scripts/Core/Get-ToggleStatus.ps1
@@ -1,7 +1,8 @@
 Function Get-ToggleStatus {
 
-    Param($ToggleSwitch)
+    Param($ToggleSwitch) # Parameter to specify which toggle switch status to check
 
+    # Check status of "ToggleDarkMode"
     if($ToggleSwitch -eq "ToggleDarkMode"){
         $app = (Get-ItemProperty -path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize').AppsUseLightTheme
         $system = (Get-ItemProperty -path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize').SystemUsesLightTheme
@@ -9,46 +10,55 @@ Function Get-ToggleStatus {
             return $true
         }
         else{
+            # Return true if Sticky Keys are enabled
             return $false
         }
     }
   
+    # Check status of "ToggleShowExt" (Show File Extensions)
     if($ToggleSwitch -eq "ToggleShowExt"){
         $hideextvalue = (Get-ItemProperty -path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced').HideFileExt
         if($hideextvalue -eq 0){
             return $true
         }
         else{
+            # Return true if Sticky Keys are enabled
             return $false
         }
     }
 
+    # Check status of "ToggleShowHidden" (Show Hidden Files)
     if($ToggleSwitch -eq "ToggleShowHidden"){
         $hideextvalue = (Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSuperHidden")
         if($hideextvalue -eq 1){
             return $true
         }
         else{
+            # Return true if Sticky Keys are enabled
             return $false
         }
     }
 
+    # Check status of "ToggleNumLock"
     if($ToggleSwitch -eq "ToggleNumLook"){
         $numlockvalue = (Get-ItemProperty -path 'HKCU:\Control Panel\Keyboard').InitialKeyboardIndicators
         if($numlockvalue -eq 2){
             return $true
         }
         else{
+            # Return true if Sticky Keys are enabled
             return $false
         }
     } 
     
+    # Check status of "ToggleStickyKeys"    
     if ($ToggleSwitch -eq "ToggleStickyKeys") {
         $StickyKeys = (Get-ItemProperty -path 'HKCU:\Control Panel\Accessibility\StickyKeys').Flags
         if($StickyKeys -eq 58){
             return $false
         }
         else{
+            # Return true if Sticky Keys are enabled
             return $true
         }
     }
