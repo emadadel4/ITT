b05fe9129cae761237bfab3c4d0876952fd59223
Emad Adel <emadadel1920@gmail.com>
2024-08-13 07:45:29 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -20,7 +20,7 @@ Add-Type -AssemblyName System.Windows.Forms
 $sync = [Hashtable]::Synchronized(@{
     database       = @{}
     ProcessRunning = $false
-    lastupdate     = "09/01/24"
+    lastupdate     = "08/13/24"
     github         = "https://github.com/emadadel4"
     telegram       = "https://t.me/emadadel4"
     website        = "https://emadadel4.github.io"
