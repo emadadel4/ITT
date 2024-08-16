78808e1d8791c7f4515bf80c905f74d70894d9eb
Emad Adel <emadadel1920@gmail.com>
2024-08-16 04:14:50 +0300

    Update Play-Music.ps1

--- Scripts/UI/Play-Music.ps1
+++ Scripts/UI/Play-Music.ps1
@@ -1,45 +1,40 @@
 function PlayMusic {
 
-    Invoke-ScriptBlock -ScriptBlock {
-
-        # Function to play an audio track
-        function PlayAudio($track) {
-            $mediaItem = $sync.mediaPlayer.newMedia($track)
-            $sync.mediaPlayer.currentPlaylist.appendItem($mediaItem)
-            $sync.mediaPlayer.controls.play()
-        }
-
-        # Shuffle the playlist and create a new playlist
-        function GetShuffledTracks {
-
-            # Play Favorite Music in Special Date
-            if ($sync.Date.Month -eq 9 -and $sync.Date.Day -eq 1) {
-                return $sync.database.OST.Favorite | Get-Random -Count $sync.database.OST.Favorite.Count
-            }
-            else {
-                return $sync.database.OST.Tracks | Get-Random -Count $sync.database.OST.Tracks.Count
-            }
-        }
-
-        # Preload and play the shuffled playlist
-        function PlayPreloadedPlaylist {
-            # Preload the shuffled playlist
-            $shuffledTracks = GetShuffledTracks
-
-            while ($true) {  # Loop indefinitely to repeat the preloaded playlist
-                foreach ($track in $shuffledTracks) {
-                    PlayAudio -track $track.url
-                    # Wait for the track to finish playing
-                    while ($sync.mediaPlayer.playState -in 3, 6) {
-                        Start-Sleep -Milliseconds 100
-                    }
-                }
-            }
-        }
-
-        # Play the preloaded playlist
-        PlayPreloadedPlaylist
+    # Function to play an audio track
+    function PlayAudio($track) {
+        $mediaItem = $sync.mediaPlayer.newMedia($track)
+        $sync.mediaPlayer.currentPlaylist.appendItem($mediaItem)
+        $sync.mediaPlayer.controls.play()
     }
+
+    # Shuffle the playlist and create a new playlist
+    function GetShuffledTracks {
+
+        # Play Favorite Music in Special Date
+        if ($sync.Date.Month -eq 9 -and $sync.Date.Day -eq 1) {
+            return $sync.database.OST.Favorite | Get-Random -Count $sync.database.OST.Favorite.Count
+        }
+        else {
+            return $sync.database.OST.Tracks | Get-Random -Count $sync.database.OST.Tracks.Count
+        }
+    }
+
+    # Preload and play the shuffled playlist
+    function PlayPreloadedPlaylist {
+        # Preload the shuffled playlist
+        $shuffledTracks = GetShuffledTracks
+
+        foreach ($track in $shuffledTracks) {
+            PlayAudio -track $track.url
+            # Wait for the track to finish playing
+            while ($sync.mediaPlayer.playState -in 3, 6) {
+                Start-Sleep -Milliseconds 100
+            }
+        }
+    }
+
+    # Play the preloaded playlist
+    PlayPreloadedPlaylist
 }
 
 # Mute the music by setting the volume to the specified value
