ed759fee72844ae66d205483ebc4535a2eb9d38c
Emad Adel <emadadel1920@gmail.com>
2024-08-23 06:07:52 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -12298,7 +12298,7 @@ $childXaml = '<Window
       <StackPanel Orientation="Vertical">
           
         <!--Logo-->
-              <Image Source="https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Images/logo.png" Margin="10" Height="80" Width="Auto"/>
+              <Image Source="https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Images/logo2.png" Margin="10" Height="80" Width="Auto"/>
         <!--End Logo-->
         
         <TextBlock Text="Install Tweak Tool" 
