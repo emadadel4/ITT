26157a298eb0e350906822551eeab86634c20afe
Emad Adel <emadadel1920@gmail.com>
2024-08-24 06:43:01 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -42,7 +42,7 @@ $administrator = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 
 if (-not $principal.IsInRole($administrator))
 {
-    Start-Process -FilePath "PowerShell" -ArgumentList $myInvocation.MyCommand.Definition -Verb "runas"
+    Start-Process -FilePath "PowerShell" -Verb "runas"
     break
     exit
 }
