b395ef25adb47be569fcbd30be63bc63ff4f217e
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-08-31 04:46:38 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -13258,23 +13258,18 @@ function WriteAText {
         $color
     )
     
-    Write-Host " +==============================================================================+" -ForegroundColor Yellow;
-    Write-Host " |   ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _       |" -ForegroundColor Yellow;
-    Write-Host " |  |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |      |" -ForegroundColor Yellow;
-    Write-Host " |   | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |      |" -ForegroundColor Yellow;
-    Write-Host " |   | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___   |" -ForegroundColor Yellow;
-    Write-Host " |  |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|  |" -ForegroundColor Yellow;
-    Write-Host " |                       Made with ♥ By Emad Adel                               |" -ForegroundColor Yellow;
-    Write-Host " |                          #StandWithPalestine                                 |" -ForegroundColor Yellow;
-    Write-Host " +==============================================================================+" -ForegroundColor Yellow; 
-    Write-Host " $message `n` " -ForegroundColor Yellow
-    Write-Host " Github repo: https://github.com/emadadel4/ITT/issues" -ForegroundColor Yellow
-    Write-Host " Telegram: https://t.me/ittemadadel" -ForegroundColor Yellow
-    Write-Host " Discord: https://discord.com/invite/3eV79KgD" -ForegroundColor Yellow
-    Write-Host " Commands:" -ForegroundColor Yellow
-    Write-Host " irm bit.ly/ittea | iex" -ForegroundColor Yellow
-    Write-Host " irm cutt.ly/ittea | iex" -ForegroundColor Yellow
-    Write-Host " irm bit.ly/emadadel | iex" -ForegroundColor Yellow
+    Write-Host " +==============================================================================+" 
+    Write-Host " |   ___ _____ _____   _____ __  __    _    ____       _    ____  _____ _       |" 
+    Write-Host " |  |_ _|_   _|_   _| | ____|  \/  |  / \  |  _ \     / \  |  _ \| ____| |      |" 
+    Write-Host " |   | |  | |   | |   |  _| | |\/| | / _ \ | | | |   / _ \ | | | |  _| | |      |" 
+    Write-Host " |   | |  | |   | |   | |___| |  | |/ ___ \| |_| |  / ___ \| |_| | |___| |___   |" 
+    Write-Host " |  |___| |_|   |_|   |_____|_|  |_/_/   \_\____/  /_/   \_\____/|_____|_____|  |" 
+    Write-Host " |                       Made with ♥ By Emad Adel                               |" 
+    Write-Host " |                          #StandWithPalestine                                 |" 
+    Write-Host " +==============================================================================+" 
+    Write-Host " $message `n` " 
+    Write-Host " Telegram: https://t.me/ittemadadel" 
+    Write-Host " Discord: https://discord.com/invite/3eV79KgD"
 }
 function Get-PCInfo {
     param (
