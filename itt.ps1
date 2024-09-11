b22d4e1e10504f2f52d887dabc7bffe641e02e09
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-12 02:34:24 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -13929,12 +13929,8 @@ function Invoke-ApplyTweaks {
                                
                             try {
                                 Add-Log -Message "Trying to remove $($Name)" -Level "INFO"
-                                Start-Process -FilePath "powershell" -ArgumentList {
-                                    Get-AppxPackage "*$Name*" | Remove-AppxPackage -ErrorAction SilentlyContinue
-                                } -NoNewWindow -Wait -PassThru
-
-                                # Get-AppxPackage "*$Name*" | Remove-AppxPackage -ErrorAction SilentlyContinue
-                                # Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$Name*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
+                                Get-AppxPackage "*$Name*" | Remove-AppxPackage -ErrorAction SilentlyContinue
+                                Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$Name*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue
                             } catch {
                                 #Write-Warning "Unable to uninstall $name"
                                 #Write-Warning $psitem.Exception.StackTrace
