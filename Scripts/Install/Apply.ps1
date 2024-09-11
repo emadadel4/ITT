e9805198c936e7362caf13ab6d2c3fbd673f471c
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-12 02:43:21 +0300

    Update Apply.ps1

--- Scripts/Install/Apply.ps1
+++ Scripts/Install/Apply.ps1
@@ -217,8 +217,7 @@ function Invoke-ApplyTweaks {
                                 Start-Process -FilePath "powershell.exe" -ArgumentList "-Command Get-AppxPackage '*$Name*' | Remove-AppxPackage -ErrorAction SilentlyContinue" -NoNewWindow -Wait
                                 Start-Process -FilePath "powershell.exe" -ArgumentList "-Command Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$Name*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue" -NoNewWindow -Wait
                             } catch {
-                                #Write-Warning "Unable to uninstall $name"
-                                #Write-Warning $psitem.Exception.StackTrace
+                                
                             }
                         }
                            
