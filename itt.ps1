a01d8e475f73682d6f60982ee0bc9068dbb8e77e
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-12 02:37:55 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -13932,7 +13932,6 @@ function Invoke-ApplyTweaks {
                                 #Get-AppxPackage "*$Name*" | Remove-AppxPackage -ErrorAction SilentlyContinue
                                 Start-Process -FilePath "powershell.exe" -ArgumentList "-Command Get-AppxPackage '*$Name*' | Remove-AppxPackage -ErrorAction SilentlyContinue" -NoNewWindow -Wait
                                 Start-Process -FilePath "powershell.exe" -ArgumentList "-Command Get-AppxProvisionedPackage -Online | Where-Object DisplayName -like "*$Name*" | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue" -NoNewWindow -Wait
-                                #
                             } catch {
                                 #Write-Warning "Unable to uninstall $name"
                                 #Write-Warning $psitem.Exception.StackTrace
