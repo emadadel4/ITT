25c2cea64b79a0489ea819b0246199731b8c170d
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-12 10:15:24 +0300

    Update Finish.ps1

--- Scripts/Core/Finish.ps1
+++ Scripts/Core/Finish.ps1
@@ -28,3 +28,27 @@ function Finish {
 
     Notify -title "$title" -msg "$msg" -icon "Info" -time 30000
 }
+
+function Clear-Item {
+    param (
+        $ListView
+    )
+
+     # Uncheck all checkboxes in $list if user chose [NO]
+     $itt.$ListView.Dispatcher.Invoke({
+        foreach ($item in $itt.$ListView.Items) {
+            $item.Children | ForEach-Object {
+                if ($_ -is [System.Windows.Controls.StackPanel]) {
+                    $_.Children | ForEach-Object {
+                        if ($_ -is [System.Windows.Controls.CheckBox]) {
+                            $_.IsChecked = $false
+                        }
+                    }
+                }
+            }
+        }
+        $itt.$ListView.Clear()
+        [System.Windows.Data.CollectionViewSource]::GetDefaultView($itt.$ListView.Items).Filter = $null
+    })
+    
+}
