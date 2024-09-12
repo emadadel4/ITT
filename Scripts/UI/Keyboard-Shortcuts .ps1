6780c696ca195720329aa620c9f64cd0b19a87f1
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-12 09:31:30 +0300

    BOptimization and Clean code

--- Scripts/UI/Keyboard-Shortcuts .ps1
+++ Scripts/UI/Keyboard-Shortcuts .ps1
@@ -1,104 +1,103 @@
-# $KeyEvents = {
+$KeyEvents = {
 
-#     if ($itt.ProcessRunning -eq $true) {
-#         return
-#     }
+    if ($itt.ProcessRunning -eq $true) {
+        return
+    }
 
-#     if (($_.Key -eq "Enter")) {
+    if (($_.Key -eq "Enter")) {
 
-#         switch ($itt.currentList) {
-#             "appslist" {
-#                 Invoke-Install                
-#             }
-#             "tweakslist" {
-#                 Invoke-ApplyTweaks
-#             }
-#         }
-#     }
+        switch ($itt.currentList) {
+            "appslist" {
+                Invoke-Install                
+            }
+            "tweakslist" {
+                Invoke-ApplyTweaks
+            }
+        }
+    }
 
-#     if (($_.Key -eq "S" -and $_.KeyboardDevice.Modifiers -eq "Ctrl")) {
+    if (($_.Key -eq "S" -and $_.KeyboardDevice.Modifiers -eq "Ctrl")) {
 
-#         switch ($itt.currentList) {
-#             "appslist" {
-#                 Invoke-Install                
-#             }
-#             "tweakslist" {
-#                 Invoke-ApplyTweaks
-#             }
-#         }
-#     }
+        switch ($itt.currentList) {
+            "appslist" {
+                Invoke-Install                
+            }
+            "tweakslist" {
+                Invoke-ApplyTweaks
+            }
+        }
+    }
 
-#      # Quit from applaction
-#      if (($_.Key -eq "G" -and $_.KeyboardDevice.Modifiers -eq "Ctrl")) {
-#         $this.Close()
-#     }
+     # Quit from applaction
+     if (($_.Key -eq "G" -and $_.KeyboardDevice.Modifiers -eq "Ctrl")) {
+        $this.Close()
+    }
 
-#     # Foucs on Search box
-#     if (($_.Key -eq "F" -and $_.KeyboardDevice.Modifiers -eq "Ctrl")) {
-#         $itt.SearchInput.Focus()
-#     }
+    # Foucs on Search box
+    if (($_.Key -eq "F" -and $_.KeyboardDevice.Modifiers -eq "Ctrl")) {
+        $itt.SearchInput.Focus()
+    }
 
-#     # Lost Foucs on Search box
-#     if ($_.Key -eq "Escape") {
-#         $itt.SearchInput.MoveFocus([System.Windows.Input.TraversalRequest]::New([System.Windows.Input.FocusNavigationDirection]::Next))
-#         $itt.SearchInput.Text = ""
-#     }
+    # Lost Foucs on Search box
+    if ($_.Key -eq "Escape") {
+        $itt.SearchInput.MoveFocus([System.Windows.Input.TraversalRequest]::New([System.Windows.Input.FocusNavigationDirection]::Next))
+        $itt.SearchInput.Text = ""
+    }
 
-#     # Swtich to Apps tap
-#     if ($_.Key -eq "Q" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
-#         $itt.TabControl.SelectedItem = $itt.TabControl.Items | Where-Object { $_.Name -eq "apps" }
-#     }
+    # Swtich to Apps tap
+    if ($_.Key -eq "Q" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
+        $itt.TabControl.SelectedItem = $itt.TabControl.Items | Where-Object { $_.Name -eq "apps" }
+    }
 
-#     # Swtich to tweaks tap
-#     if ($_.Key -eq "W" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
-#         $itt.TabControl.SelectedItem = $itt.TabControl.Items | Where-Object { $_.Name -eq "tweeksTab" }
-#     }
+    # Swtich to tweaks tap
+    if ($_.Key -eq "W" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
+        $itt.TabControl.SelectedItem = $itt.TabControl.Items | Where-Object { $_.Name -eq "tweeksTab" }
+    }
 
-#     # Swtich to settings tap
-#     if ($_.Key -eq "E" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
-#         $itt.TabControl.SelectedItem = $itt.TabControl.Items | Where-Object { $_.Name -eq "SettingsTab" }
-#     }
+    # Swtich to settings tap
+    if ($_.Key -eq "E" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
+        $itt.TabControl.SelectedItem = $itt.TabControl.Items | Where-Object { $_.Name -eq "SettingsTab" }
+    }
 
-#     # Swtich to settings tap
-#     if ($_.Key -eq "I" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
-#         About
-#     }
+    # Swtich to settings tap
+    if ($_.Key -eq "I" -and $_.KeyboardDevice.Modifiers -eq "Ctrl") {
+        About
+    }
 
-#     # SaveItemsToJson
-#     if ($_.Key -eq "S" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
-#         SaveItemsToJson
-#     }
+    # SaveItemsToJson
+    if ($_.Key -eq "S" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
+        SaveItemsToJson
+    }
 
-#     # LoadJson
-#     if ($_.Key -eq "D" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
-#         LoadJson
-#     }
+    # LoadJson
+    if ($_.Key -eq "D" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
+        LoadJson
+    }
 
-#     # Mute
-#     if ($_.Key -eq "M" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
-#         MuteMusic -Value 0
+    # Mute
+    if ($_.Key -eq "M" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
+        MuteMusic -Value 0
 
-#     }
+    }
 
-#     # Music ON 
-#     if ($_.Key -eq "F" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
-#         UnmuteMusic -Value 100
-#     }
+    # Music ON 
+    if ($_.Key -eq "F" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
+        UnmuteMusic -Value 100
+    }
 
-#     # Choco Folder
-#     if ($_.Key -eq "P" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
-#         Start-Process explorer.exe "C:\ProgramData\chocolatey\lib"
-#     }
+    # Choco Folder
+    if ($_.Key -eq "P" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
+        Start-Process explorer.exe "C:\ProgramData\chocolatey\lib"
+    }
 
-#     # Restore point 
-#     if ($_.Key -eq "Q" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
-#         RestorePoint
-#     }
+    # Restore point 
+    if ($_.Key -eq "Q" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
+        RestorePoint
+    }
 
-#     # ITT Shortcut 
-#     if ($_.Key -eq "I" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
-#         ITTShortcut
-#     }
-# }
+    # ITT Shortcut 
+    if ($_.Key -eq "I" -and $_.KeyboardDevice.Modifiers -eq "Shift") {
+        ITTShortcut
+    }
+}
 
-# $itt["window"].Add_PreViewKeyDown($KeyEvents)
