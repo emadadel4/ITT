c21bcecf5cb5be08057ced13a4165d744b3b4e5c
Emad Adel <emadadel1920@gmail.com>
2024-09-13 11:01:56 +0300

    Update Remove-Registry.ps1

--- Scripts/Core/Remove-Registry.ps1
+++ Scripts/Core/Remove-Registry.ps1
@@ -1,4 +1,28 @@
 function Remove-Registry {
+    
+    <#
+        .SYNOPSIS
+        Deletes a specified registry key and all its subkeys.
+
+        .DESCRIPTION
+        This function removes a registry key and its associated subkeys from the Windows registry. It combines the provided registry path and folder name to form the full registry key path and deletes it if it exists. The function includes error handling to manage any issues that occur during the deletion process.
+
+        .PARAMETER RegistryPath
+        The base path of the registry where the key is located. This parameter is mandatory.
+
+        .PARAMETER Folder
+        The name of the registry key (folder) to be deleted. This parameter is mandatory.
+
+        .EXAMPLE
+        Remove-Registry -RegistryPath "HKCU\Software\MyCompany" -Folder "MyApp"
+        Deletes the registry key "MyApp" located under "HKCU\Software\MyCompany" and all of its subkeys.
+
+        .NOTES
+        File Name      : Remove-Registry.ps1
+        Author          : Your Name
+        Prerequisite    : Ensure you have the necessary permissions to modify the registry.
+    #>
+
     param (
         [Parameter(Mandatory=$true)]
         [string]$RegistryPath,
