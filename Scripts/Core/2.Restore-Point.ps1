bbf0dc2d323502cc9c5a5aab7e463365bdcccee0
Emad Adel <emadadel1920@gmail.com>
2024-09-13 10:55:07 +0300

    Update 2.Restore-Point.ps1

--- Scripts/Core/2.Restore-Point.ps1
+++ Scripts/Core/2.Restore-Point.ps1
@@ -1,5 +1,18 @@
 function RestorePoint {
 
+    <#
+    .SYNOPSIS
+    Creates a system restore point on the local computer.
+
+    .DESCRIPTION
+    This function creates a system restore point using the Checkpoint-Computer cmdlet. It logs the process of creating the restore point and handles any errors that occur during the creation. If the restore point creation fails, an error message is displayed.
+
+    .EXAMPLE
+    RestorePoint
+    Creates a restore point and logs the success or failure of the operation.
+
+    #>
+
     Invoke-ScriptBlock -ScriptBlock {
 
         Try {
