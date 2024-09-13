791309159335182c6f44a63fd6dac5c9db7130ac
Emad Adel <emadadel1920@gmail.com>
2024-09-13 10:56:05 +0300

    Update Disable-Service.ps1

--- Scripts/Core/Disable-Service.ps1
+++ Scripts/Core/Disable-Service.ps1
@@ -1,5 +1,22 @@
 function Disable-Service {
     
+    <#
+    .SYNOPSIS
+    Disables a specified service by changing its startup type and stopping it.
+
+    .DESCRIPTION
+    This function disables a Windows service by first changing its startup type to the specified value, then stopping the service if it is running. The function logs the outcome of the operation, including whether the service was found and successfully disabled or if an error occurred.
+
+    .PARAMETER ServiceName
+    The name of the service to be disabled. This is a required parameter.
+
+    .PARAMETER StartupType
+    The desired startup type for the service. Common values include 'Disabled', 'Manual', and 'Automatic'. This is a required parameter.
+
+    .EXAMPLE
+    Disable-Service -ServiceName "wuauserv" -StartupType "Disabled"
+    #>
+
     param(
         $ServiceName,
         $StartupType
