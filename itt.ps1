5364e61705647dd78ff817f5f9725330dab23e32
Emad Adel <emadadel1920@gmail.com>
2024-09-15 04:17:19 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -8853,10 +8853,10 @@ function RestorePoint {
 
         Try {
             Add-Log -Message "Creating Restore point..." -Level "INFO"
-            Checkpoint-Computer -Description "ITT" -ErrorAction Stop
+            Start-Process powershell.exe -ArgumentList "-NoExit", "-Command `"Enable-ComputerRestore -Drive '$env:SystemDrive'; Checkpoint-Computer -Description 'ITT' -RestorePointType 'MODIFY_SETTINGS'; exit`"" -Wait -Verb RunAs
             Add-Log -Message "Created successfully" -Level "INFO"
         } Catch {
-            Write-Host "Failed to create a restore point. Error: $($_.Exception.Message)" -ForegroundColor Red
+            Add-Log -Message "An error occurred while enabling System Restore: $_" -Level "ERROR"
         }
     }
 }
