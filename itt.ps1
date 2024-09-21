2f6db1e1e77ce655d46a98bd245e02130d0a8a91
Emad Adel <emadadel1920@gmail.com>
2024-09-21 07:38:58 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -9797,14 +9797,6 @@ function Refresh-Explorer {
 
     # Restart the Explorer process
     Stop-Process -Name explorer -Force
-
-    # Refresh Windows Explorer
-    $shell = New-Object -ComObject Shell.Application
-    $shell.Windows() | Where-Object { $_.Name -eq 'File Explorer' } | ForEach-Object { $_.Refresh() }
-
-    # Refresh the desktop
-    $desktop = New-Object -ComObject Shell.Application
-    $desktop.NameSpace(0).Self.InvokeVerb('Refresh')
 }
 function Remove-Registry {
 
@@ -10058,6 +10050,64 @@ function Set-Registry {
 
     catch {
         Write-Error "An error occurred: $_"
+    }
+}
+function Set-Taskbar {
+
+    <#
+        .SYNOPSIS
+        Sets the taskbar progress and overlay icon in the application window.
+
+        .DESCRIPTION
+        The `Set-Taskbar` function allows setting the taskbar progress state, progress value, 
+        and overlay icon for the application's taskbar item. 
+        You can specify the progress state, value, and change the icon displayed on the taskbar.
+
+        .EXAMPLE
+        Set-Taskbar -progress 'Normal' -value 0.5 -icon 'done'
+        This example sets the taskbar progress state to 'Normal' with a progress value of 50% 
+        and shows a 'done' icon as an overlay for 2 seconds before switching back to the default icon.
+    #>
+    
+    param (
+        [string]$progress,
+        [double]$value,
+        [string]$icon
+    )
+
+
+    if ($value) {
+        $itt["window"].taskbarItemInfo.ProgressValue = $value
+    }
+
+
+    if($progress)
+    {
+        switch ($progress) {
+            'None' { $itt["window"].taskbarItemInfo.ProgressState = "None" }
+            'Normal' { $itt["window"].taskbarItemInfo.ProgressState = "Normal" }
+            'Indeterminate' { $itt["window"].taskbarItemInfo.ProgressState = "Indeterminate" }
+            'Error' { $itt["window"].taskbarItemInfo.ProgressState = "Error" }
+            default { throw "Set-Taskbar Invalid state" }
+        }
+    }
+
+    if($icon)
+    {
+        switch ($icon) {
+
+            "done" {
+                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/done.png"
+            }
+            "logo" {
+                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/icon.ico"
+            }
+            "error" {
+                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/error.png"
+                Start-Sleep -Seconds 2
+                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/icon.ico"
+            }
+        }
     }
 }
 function Startup  {
@@ -10342,64 +10392,6 @@ function ChangeTap {
     }
 }
 
-function Set-Taskbar {
-
-    <#
-        .SYNOPSIS
-        Sets the taskbar progress and overlay icon in the application window.
-
-        .DESCRIPTION
-        The `Set-Taskbar` function allows setting the taskbar progress state, progress value, 
-        and overlay icon for the application's taskbar item. 
-        You can specify the progress state, value, and change the icon displayed on the taskbar.
-
-        .EXAMPLE
-        Set-Taskbar -progress 'Normal' -value 0.5 -icon 'done'
-        This example sets the taskbar progress state to 'Normal' with a progress value of 50% 
-        and shows a 'done' icon as an overlay for 2 seconds before switching back to the default icon.
-    #>
-    
-    param (
-        [string]$progress,
-        [double]$value,
-        [string]$icon
-    )
-
-
-    if ($value) {
-        $itt["window"].taskbarItemInfo.ProgressValue = $value
-    }
-
-
-    if($progress)
-    {
-        switch ($progress) {
-            'None' { $itt["window"].taskbarItemInfo.ProgressState = "None" }
-            'Normal' { $itt["window"].taskbarItemInfo.ProgressState = "Normal" }
-            'Indeterminate' { $itt["window"].taskbarItemInfo.ProgressState = "Indeterminate" }
-            'Error' { $itt["window"].taskbarItemInfo.ProgressState = "Error" }
-            default { throw "Set-Taskbar Invalid state" }
-        }
-    }
-
-    if($icon)
-    {
-        switch ($icon) {
-
-            "done" {
-                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/done.png"
-            }
-            "logo" {
-                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/icon.ico"
-            }
-            "error" {
-                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/error.png"
-                Start-Sleep -Seconds 2
-                $itt["window"].taskbarItemInfo.Overlay = "https://raw.githubusercontent.com/emadadel4/ITT/main/Resources/Icons/icon.ico"
-            }
-        }
-    }
-}
 function Uninstall-AppxPackage  {
     <#
         .SYNOPSIS
