3611c6a18b482fcffc467427e51b88194cf0b06d
Emad Adel <emadadel1920@gmail.com>
2024-09-22 10:09:25 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -1,11 +1,11 @@
 
+$url = "https://github.com/emadadel4/itt/releases/latest/download/itt.ps1"
 $currentPid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = [System.Security.Principal.WindowsPrincipal]$currentPid
 $administrator = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 
 if (-not $principal.IsInRole($administrator))
 {
-    $url = "https://github.com/emadadel4/itt/releases/latest/download/itt.ps1"
     $script = Invoke-RestMethod $url
     $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
     $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
