f21a81fc8ea6c8da80cd2c424b7fab3e9d0326a2
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-22 10:55:35 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -1,10 +1,19 @@
 $url = "https://github.com/emadadel4/itt/releases/latest/download/ittea.ps1"
-$currentPid = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$principal = [System.Security.Principal.WindowsPrincipal]$currentPid
-$administrator = [System.Security.Principal.WindowsBuiltInRole]::Administrator
 
-if (-not $principal.IsInRole($administrator))
-{
-    Start-Process -FilePath "PowerShell" -ArgumentList "irm $url | iex" -Verb "runas"
-    exit
+$currentIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object System.Security.Principal.WindowsPrincipal($currentIdentity)
+$administratorRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+
+$script = $url
+
+if (-not $principal.IsInRole($administratorRole)) {
+    # Determine which PowerShell version and terminal to use
+    $powershellcmd = if (Get-Command pwsh -ErrorAction SilentlyContinue) { "pwsh" } else { "powershell" }
+    $processCmd = if (Get-Command wt.exe -ErrorAction SilentlyContinue) { "wt.exe" } else { $powershellcmd }
+
+    # Elevate the process using RunAs
+    Start-Process $processCmd -ArgumentList "$powershellcmd -ExecutionPolicy Bypass -NoProfile -Command `"irm $script | iex`"" -Verb RunAs
+} else {
+    # Run the script directly if already elevated
+    irm $script | iex
 }
