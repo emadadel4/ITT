5ea469141c34fb727e4ff63a4b3f2b1231239b61
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-23 03:30:55 +0300

    Update Refresh-Explorer.ps1

--- Scripts/Core/Refresh-Explorer.ps1
+++ Scripts/Core/Refresh-Explorer.ps1
@@ -1,11 +1,18 @@
 function Refresh-Explorer {
     
-    # Stop the Explorer process
-    Stop-Process -Name explorer -Force
+    Invoke-ScriptBlock -ScriptBlock {
 
-    # Wait a moment to ensure the process has stopped
-    Start-Sleep -Seconds 1
+        $itt.ProcessRunning = $true
 
-    # Restart the Explorer process
-    Stop-Process -Name explorer -Force
+        # Stop the Explorer process
+        Stop-Process -Name explorer -Force
+
+        # Wait a moment to ensure the process has stopped
+        Start-Sleep -Seconds 1
+
+        # Restart the Explorer process
+        Stop-Process -Name explorer -Force
+
+        $itt.ProcessRunning = $false
+    }
 }
