53ad1930e28231a3629b18406f88bec43a575c45
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-09-23 03:23:40 +0300

    Update About-Dev.ps1

--- Scripts/UI/About-Dev.ps1
+++ Scripts/UI/About-Dev.ps1
@@ -1,19 +1,24 @@
 function About {
-    # Load child window
+
+    # init child window
     [xml]$about = $childXaml
     $childWindowReader = (New-Object System.Xml.XmlNodeReader $about)
     $itt.about = [Windows.Markup.XamlReader]::Load($childWindowReader)
+
+    # Get main style theme
     $itt["about"].Resources.MergedDictionaries.Add($itt["window"].FindResource($itt.CurretTheme))
-    # Set version and link handlers
+
+    # Set Events on Click
     $itt.about.FindName('ver').Text = "Last update $($itt.lastupdate)"
-    $itt.about.FindName("telegram").add_MouseLeftButtonDown({Start-Process("https://t.me/emadadel4")})
-    $itt.about.FindName("github").add_MouseLeftButtonDown({Start-Process("https://github.com/emadadel4/itt")})
-    $itt.about.FindName("blog").add_MouseLeftButtonDown({Start-Process("https://emadadel4.github.io")})
-    $itt.about.FindName("yt").add_MouseLeftButtonDown({Start-Process("https://youtube.com/@emadadel4")})
-    $itt.about.FindName("coffee").add_MouseLeftButtonDown({Start-Process("https://buymeacoffee.com/emadadel")})
-    # Set data context based on language
+    $itt.about.FindName("telegram").add_MouseLeftButtonDown({Start-Process($itt.telegram)})
+    $itt.about.FindName("github").add_MouseLeftButtonDown({Start-Process($itt.github)})
+    $itt.about.FindName("blog").add_MouseLeftButtonDown({Start-Process($itt.blog)})
+    $itt.about.FindName("yt").add_MouseLeftButtonDown({Start-Process($itt.youtube)})
+    $itt.about.FindName("coffee").add_MouseLeftButtonDown({Start-Process($itt.buymeacoffee)})
     
+    # Set data context language
     $itt.about.DataContext = $itt.database.locales.Controls.en
 
+    # Show window
     $itt.about.ShowDialog() | Out-Null
 }
