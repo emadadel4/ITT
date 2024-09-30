c97d29f4042d9e50dcd0f439d3f5f2059e887f56
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-01 02:54:35 +0300

    Update Refresh-Explorer.ps1

--- Scripts/Core/Refresh-Explorer.ps1
+++ Scripts/Core/Refresh-Explorer.ps1
@@ -1,20 +1,8 @@
 function Refresh-Explorer {
-
-    if($itt.ProcessRunning) {
-        Message -key "Pleasewait" -icon "Warning"
-        return
-    }
-
-    $itt.ProcessRunning = $true
-
     # Check if explorer is not running and start it if needed
     Stop-Process -Name explorer -Force
     Start-Sleep -Seconds 1
-
     if (-not (Get-Process -Name explorer -ErrorAction SilentlyContinue)) {
         Start-Process explorer.exe -Verb RunAs
     }
-
-    $itt.ProcessRunning = $false
-
 }
