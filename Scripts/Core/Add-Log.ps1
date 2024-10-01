fc0d4da5338d1f9c00cdbc63cc06d0b3e2244f6b
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-01 04:41:59 +0300

    Update Add-Log.ps1

--- Scripts/Core/Add-Log.ps1
+++ Scripts/Core/Add-Log.ps1
@@ -21,8 +21,8 @@ function Add-Log {
         "INFO" { $icon = "!" }
         "WARNING" { $icon = "!" }
         "ERROR" { $icon = "X" }
-        "Installed" { $icon = "✔ " }
-        "Apply" { $icon = "✔ " }
+        "Installed" { $icon = "√" }
+        "Apply" { $icon = "√" }
         default { $icon = "!" }
     }
 
