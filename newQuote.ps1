bab759bd16c12fb4ab5e1f8db690621719ff7832
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-07 08:21:26 +0300

    Add dynamic icon display and quote formatting based on quote type

--- newQuote.ps1
+++ newQuote.ps1
@@ -14,17 +14,49 @@ Write-Host "
     $jsonFilePath = "./Resources/Database/Quotes.json"
     $existingData = Get-Content $jsonFilePath -Raw -ErrorAction Stop | ConvertFrom-Json
 
-    # Prompt for input
-    $newQ = Read-Host "Enter quote"
+    $QuotesList = @{
+        # Available options
+        1 = "quote"
+        2 = "info" 
+    }
+    
+    # Prompt user to choose mothed
+    do {
 
-    # store input
-    $q = $newQ
+        Write-Host "Select text type"
+        foreach ($key in $QuotesList.Keys | Sort-Object) {
+            Write-Host "$key - $($QuotesList[$key])"
+        }
+        $choice = Read-Host "Enter the number corresponding to the methods"
+        if ([int]$choice -in $QuotesList.Keys) {
+            $type = $QuotesList[[int]$choice]
+        } else {
+            Write-Host "Invalid choice. Please select a valid option."
+        }
+    } until ([int]$choice -in $QuotesList.Keys)
+
+
+      # Prompt for input
+      $text = Read-Host "Enter text"
+      $name = Read-Host "Enter author name or source -You can skip this"
+
+
+        # Store input
+        $Quotes = @{
+            type = $type
+            text  = $text
+    }
+
+    # Add name only if it's not empty
+    if (-not [string]::IsNullOrWhiteSpace($name)) {
+        $Quotes.name = $name
+    }
 
     # Add new software object to existing array
-    $existingData.Q += $q
+    $existingData.Quotes += $Quotes
 
     # Write updated JSON to file
-    $existingData | ConvertTo-Json | Out-File $jsonFilePath -ErrorAction Stop
+    $existingData | ConvertTo-Json -Depth 4 | Out-File $jsonFilePath -ErrorAction Stop
     Write-Host  "Added Successfully" -ForegroundColor Green
 
 }
