c526eabb4e313b90311140b58313573d0b0fe467
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-11 12:12:49 +0300

    Update Play-Music.ps1

--- Scripts/UI/Play-Music.ps1
+++ Scripts/UI/Play-Music.ps1
@@ -31,7 +31,7 @@ function MuteMusic {
     $itt.mediaPlayer.settings.volume = $value
     # Save the volume setting to the registry for persistence
     Set-ItemProperty -Path $itt.registryPath -Name "Music" -Value "$value" -Force
-    $itt["window"].title = "Install Tweak Tool #StandWithPalestine 🔈"
+    $itt["window"].title = "Install Tweaks Tool #StandWithPalestine 🔈"
 
 }
 
@@ -41,7 +41,7 @@ function UnmuteMusic {
     $itt.mediaPlayer.settings.volume = $value
     # Save the volume setting to the registry for persistence
     Set-ItemProperty -Path $itt.registryPath -Name "Music" -Value "$value" -Force
-    $itt["window"].title = "Install Tweak Tool #StandWithPalestine 🔊"
+    $itt["window"].title = "Install Tweaks Tool #StandWithPalestine 🔊"
 
 }
 
