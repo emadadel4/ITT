8f717e9ad6f75eb8888e49b7ad728160fb1d98d0
Emad Adel <emadadel1920@gmail.com>
2024-10-17 03:25:38 +0300

    Update build.ps1
    Signed-off-by: Emad Adel <emadadel1920@gmail.com>

--- build.ps1
+++ build.ps1
@@ -14,7 +14,7 @@ param (
     [switch]$code,
     [string]$ProjectDir = $PSScriptRoot,
     [string]$localNodePath = "releasenotes.md",
-    [string]$NoteUrl = "https://raw.githubusercontent.com/emadadel4/ITT/refs/heads/main/releasenotes.md"
+    [string]$NoteUrl = "https://raw.githubusercontent.com/emadadel4/ITT/refs/heads/main/Changelog.md"
 
 
 )
