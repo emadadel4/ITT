8c9f6c2e80373c336e6e57c42d4f162348cbacd2
Emad Adel <19177373+emadadel4@users.noreply.github.com>
2024-10-18 09:13:46 +0300

    Update itt.ps1

--- itt.ps1
+++ itt.ps1
@@ -29,7 +29,7 @@ $itt = [Hashtable]::Synchronized(@{
     database       = @{}
     ProcessRunning = $false
     developer      = "Emad Adel"
-    lastupdate     = "10/17/2024"
+    lastupdate     = "10/18/2024"
     github         = "https://github.com/emadadel4/itt"
     telegram       = "https://t.me/emadadel4"
     blog           = "https://emadadel4.github.io"
@@ -9523,6 +9523,254 @@ $itt.database.Tweaks = '[
 #===========================================================================
 #region Begin Main Functions
 #===========================================================================
+function Invoke-Button {
+
+    <#
+        .SYNOPSIS
+        Handles various button actions and commands based on the specified action parameter.
+
+        .DESCRIPTION
+        The `Invoke-Button` function executes different actions depending on the input parameter. It manages operations such as installing apps, applying tweaks, changing themes, opening system utilities, and managing language settings. This function is designed to be used with UI elements where each button triggers a specific action.
+
+        .PARAMETER action
+        A string specifying the action to perform. The action can be one of several predefined values representing different operations, such as installing apps, applying tweaks, opening system utilities, changing themes, or managing language settings.
+
+        .EXAMPLE
+        Invoke-Button -action "installBtn"
+
+        .EXAMPLE
+        Invoke-Button -action "Dark"
+
+        .EXAMPLE
+        Invoke-Button -action "sysinfo"
+
+        .NOTES
+        - The function uses a `Switch` statement to handle different actions based on the `$action` parameter.
+        - For UI-related actions, such as installing apps or applying tweaks, it calls `Invoke-Install` or `Invoke-Apply`.
+        - For system utilities and settings, it uses `Start-Process` to open tools like Device Manager, Task Manager, or disk management utilities.
+        - For language settings, it invokes the `Set-Language` function with the specified language code.
+        - For theme changes, it calls functions like `Switch-ToDarkMode` or `Switch-ToLightMode`.
+        - For managing audio settings, it calls `MuteMusic` or `UnmuteMusic`.
+        - For opening URLs related to tools or scripts, it uses `Start-Process` with the URL as an argument.
+        - The `Debug-Message` function is used for internal debugging and can be uncommented for logging purposes.
+    #>
+
+    Param ([string]$action,[string]$Content)
+
+    # Helper function for debugging
+    function Debug-Message {
+        if($Debug) {  Add-Log "Name:$action Content:$Content" -Level "INFO"  }
+    }
+
+    # Switch block to handle different actions
+    Switch -Wildcard ($action) {
+
+        "installBtn" {
+            $itt.SearchInput.Text = $null
+            Invoke-Install
+            Debug-Message
+        }
+        "applyBtn" {
+            Invoke-Apply
+            Debug-Message 
+        }
+        "taps" {
+            ChangeTap
+        }
+        "category" {
+            FilterByCat($itt.category.SelectedItem.Content)
+            Debug-Message $action
+        }
+        "searchInput" {
+            Search
+            $itt['window'].FindName('category').SelectedIndex = 0
+            Debug-Message
+        }
+
+        # Menu items
+        "ar" {
+            Set-Language -lang "ar"
+            Debug-Message
+        }
+        "en" {
+            Set-Language -lang "en"
+            Debug-Message
+        }
+        "fr" {
+            Set-Language -lang "fr"
+            Debug-Message
+        }
+        "tr" {
+            Set-Language -lang "tr"
+            Debug-Message
+        }
+        "zh" {
+            Set-Language -lang "zh"
+            Debug-Message
+        }
+        "ko" {
+            Set-Language -lang "ko"
+            Debug-Message
+        }
+        "de" {
+            Set-Language -lang "de"
+            Debug-Message
+        }
+        "ru" {
+            Set-Language -lang "ru"
+            Debug-Message
+        }
+        "es" {
+            Set-Language -lang "es"
+            Debug-Message
+        }
+        "save" {
+            SaveItemsToJson
+            Debug-Message
+        }
+        "load" {
+            LoadJson
+            Debug-Message
+        }
+
+        # Device Management
+        "deviceManager" {
+            Start-Process devmgmt.msc 
+            Debug-Message
+        }
+        "appsfeatures" {
+            Start-Process appwiz.cpl 
+            Debug-Message
+        }
+        "sysinfo" {
+            Start-Process msinfo32.exe
+            Start-Process dxdiag.exe 
+            Debug-Message
+        }
+        "poweroption" {
+            Start-Process powercfg.cpl 
+            Debug-Message $action
+        }
+        "services" {
+            Start-Process services.msc 
+            Debug-Message $action
+        }
+        "network" {
+            Start-Process ncpa.cpl
+            Debug-Message $action
+        }
+        "taskmgr" {
+            Start-Process taskmgr.exe 
+            Debug-Message $action
+        }
+        "diskmgmt" {
+            Start-Process diskmgmt.msc
+            Debug-Message $action
+        }
+
+        "systheme" {
+            SwitchToSystem 
+            Debug-Message
+        }
+
+                    "Dark" {
+                Set-Theme -Theme $action  # Call the Set-Theme function with the selected theme
+                Debug-Message  # Output a debug message for tracking
+            }
+            "Light" {
+                Set-Theme -Theme $action  # Call the Set-Theme function with the selected theme
+                Debug-Message  # Output a debug message for tracking
+            }
+            "thedarkknight" {
+                Set-Theme -Theme $action  # Call the Set-Theme function with the selected theme
+                Debug-Message  # Output a debug message for tracking
+            }
+
+
+
+        # chocoloc
+         "chocoloc" {
+            Start-Process explorer.exe "C:\ProgramData\chocolatey\lib"
+            Debug-Message $action
+        }
+
+        # restore point
+        "restorepoint" {
+            RestorePoint
+            Debug-Message $action
+        }
+
+        # Music
+        "moff" {
+            MuteMusic -Value 0
+            Debug-Message $action
+        }
+        "mon" {
+            UnmuteMusic -Value 100
+            Debug-Message $action
+        }
+
+        # Mirror Links
+        "unhook" {
+            Start-Process "https://unhook.app/" 
+            Debug-Message $action
+        }
+        "uBlock" {
+            Start-Process "https://ublockorigin.com/" 
+            Debug-Message $action
+        }
+        "mas" {
+            Start-Process "https://github.com/massgravel/Microsoft-Activation-Scripts"
+            Debug-Message $action
+        }
+        "idm" {
+            Start-Process "https://github.com/WindowsAddict/IDM-Activation-Script"
+            Debug-Message $action
+        }
+        "neat" {
+            Start-Process "https://addons.mozilla.org/en-US/firefox/addon/neatdownloadmanager-extension/" 
+            Debug-Message $action
+        }
+
+        "winoffice" {
+            Start-Process "https://massgrave.dev/genuine-installation-media" 
+            Debug-Message $action
+        }
+        "sordum" {
+            Start-Process "https://www.sordum.org/" 
+            Debug-Message $action
+        }
+        "majorgeeks" {
+            Start-Process "https://www.majorgeeks.com/" 
+            Debug-Message $action
+        }
+
+        # Other actions
+        "ittshortcut" {
+            ITTShortcut $action
+            Debug-Message $action
+        }
+        "dev" {
+            About
+            Debug-Message $action
+        }
+        # Reset-Preferences
+
+        "reset"{
+            Reset-Preferences
+            Debug-Message $action
+        }
+
+        "shelltube"{
+            Start-Process -FilePath "powershell" -ArgumentList "irm https://github.com/emadadel4/shelltube/releases/latest/download/st.ps1 | iex"
+            Debug-Message $action
+        }
+
+        Default {
+            Write-Host "Unknown action: $action"
+        }
+    }
+}
 function Invoke-ScriptBlock {
     <#
         .SYNOPSIS
@@ -11366,254 +11614,6 @@ function Invoke-Apply {
         $itt.ProcessRunning = $false
     }
 }
-function Invoke-Button {
-
-    <#
-        .SYNOPSIS
-        Handles various button actions and commands based on the specified action parameter.
-
-        .DESCRIPTION
-        The `Invoke-Button` function executes different actions depending on the input parameter. It manages operations such as installing apps, applying tweaks, changing themes, opening system utilities, and managing language settings. This function is designed to be used with UI elements where each button triggers a specific action.
-
-        .PARAMETER action
-        A string specifying the action to perform. The action can be one of several predefined values representing different operations, such as installing apps, applying tweaks, opening system utilities, changing themes, or managing language settings.
-
-        .EXAMPLE
-        Invoke-Button -action "installBtn"
-
-        .EXAMPLE
-        Invoke-Button -action "Dark"
-
-        .EXAMPLE
-        Invoke-Button -action "sysinfo"
-
-        .NOTES
-        - The function uses a `Switch` statement to handle different actions based on the `$action` parameter.
-        - For UI-related actions, such as installing apps or applying tweaks, it calls `Invoke-Install` or `Invoke-Apply`.
-        - For system utilities and settings, it uses `Start-Process` to open tools like Device Manager, Task Manager, or disk management utilities.
-        - For language settings, it invokes the `Set-Language` function with the specified language code.
-        - For theme changes, it calls functions like `Switch-ToDarkMode` or `Switch-ToLightMode`.
-        - For managing audio settings, it calls `MuteMusic` or `UnmuteMusic`.
-        - For opening URLs related to tools or scripts, it uses `Start-Process` with the URL as an argument.
-        - The `Debug-Message` function is used for internal debugging and can be uncommented for logging purposes.
-    #>
-
-    Param ([string]$action,[string]$Content)
-
-    # Helper function for debugging
-    function Debug-Message {
-        if($Debug) {  Add-Log "Name:$action Content:$Content" -Level "INFO"  }
-    }
-
-    # Switch block to handle different actions
-    Switch -Wildcard ($action) {
-
-        "installBtn" {
-            $itt.SearchInput.Text = $null
-            Invoke-Install
-            Debug-Message
-        }
-        "applyBtn" {
-            Invoke-Apply
-            Debug-Message 
-        }
-        "taps" {
-            ChangeTap
-        }
-        "category" {
-            FilterByCat($itt.category.SelectedItem.Content)
-            Debug-Message $action
-        }
-        "searchInput" {
-            Search
-            $itt['window'].FindName('category').SelectedIndex = 0
-            Debug-Message
-        }
-
-        # Menu items
-        "ar" {
-            Set-Language -lang "ar"
-            Debug-Message
-        }
-        "en" {
-            Set-Language -lang "en"
-            Debug-Message
-        }
-        "fr" {
-            Set-Language -lang "fr"
-            Debug-Message
-        }
-        "tr" {
-            Set-Language -lang "tr"
-            Debug-Message
-        }
-        "zh" {
-            Set-Language -lang "zh"
-            Debug-Message
-        }
-        "ko" {
-            Set-Language -lang "ko"
-            Debug-Message
-        }
-        "de" {
-            Set-Language -lang "de"
-            Debug-Message
-        }
-        "ru" {
-            Set-Language -lang "ru"
-            Debug-Message
-        }
-        "es" {
-            Set-Language -lang "es"
-            Debug-Message
-        }
-        "save" {
-            SaveItemsToJson
-            Debug-Message
-        }
-        "load" {
-            LoadJson
-            Debug-Message
-        }
-
-        # Device Management
-        "deviceManager" {
-            Start-Process devmgmt.msc 
-            Debug-Message
-        }
-        "appsfeatures" {
-            Start-Process appwiz.cpl 
-            Debug-Message
-        }
-        "sysinfo" {
-            Start-Process msinfo32.exe
-            Start-Process dxdiag.exe 
-            Debug-Message
-        }
-        "poweroption" {
-            Start-Process powercfg.cpl 
-            Debug-Message $action
-        }
-        "services" {
-            Start-Process services.msc 
-            Debug-Message $action
-        }
-        "network" {
-            Start-Process ncpa.cpl
-            Debug-Message $action
-        }
-        "taskmgr" {
-            Start-Process taskmgr.exe 
-            Debug-Message $action
-        }
-        "diskmgmt" {
-            Start-Process diskmgmt.msc
-            Debug-Message $action
-        }
-
-        "systheme" {
-            SwitchToSystem 
-            Debug-Message
-        }
-
-                    "Dark" {
-                Set-Theme -Theme $action
-                Debug-Message
-            }
-            "Light" {
-                Set-Theme -Theme $action
-                Debug-Message
-            }
-            "thedarkknight" {
-                Set-Theme -Theme $action
-                Debug-Message
-            }
-
-
-
-        # chocoloc
-         "chocoloc" {
-            Start-Process explorer.exe "C:\ProgramData\chocolatey\lib"
-            Debug-Message $action
-        }
-
-        # restore point
-        "restorepoint" {
-            RestorePoint
-            Debug-Message $action
-        }
-
-        # Music
-        "moff" {
-            MuteMusic -Value 0
-            Debug-Message $action
-        }
-        "mon" {
-            UnmuteMusic -Value 100
-            Debug-Message $action
-        }
-
-        # Mirror Links
-        "unhook" {
-            Start-Process "https://unhook.app/" 
-            Debug-Message $action
-        }
-        "uBlock" {
-            Start-Process "https://ublockorigin.com/" 
-            Debug-Message $action
-        }
-        "mas" {
-            Start-Process "https://github.com/massgravel/Microsoft-Activation-Scripts"
-            Debug-Message $action
-        }
-        "idm" {
-            Start-Process "https://github.com/WindowsAddict/IDM-Activation-Script"
-            Debug-Message $action
-        }
-        "neat" {
-            Start-Process "https://addons.mozilla.org/en-US/firefox/addon/neatdownloadmanager-extension/" 
-            Debug-Message $action
-        }
-
-        "winoffice" {
-            Start-Process "https://massgrave.dev/genuine-installation-media" 
-            Debug-Message $action
-        }
-        "sordum" {
-            Start-Process "https://www.sordum.org/" 
-            Debug-Message $action
-        }
-        "majorgeeks" {
-            Start-Process "https://www.majorgeeks.com/" 
-            Debug-Message $action
-        }
-
-        # Other actions
-        "ittshortcut" {
-            ITTShortcut $action
-            Debug-Message $action
-        }
-        "dev" {
-            About
-            Debug-Message $action
-        }
-        # Reset-Preferences
-
-        "reset"{
-            Reset-Preferences
-            Debug-Message $action
-        }
-
-        "shelltube"{
-            Start-Process -FilePath "powershell" -ArgumentList "irm https://github.com/emadadel4/shelltube/releases/latest/download/st.ps1 | iex"
-            Debug-Message $action
-        }
-
-        Default {
-            Write-Host "Unknown action: $action"
-        }
-    }
-}
 function Invoke-Toogle {
 
     <#
@@ -12633,52 +12633,6 @@ function Set-Theme {
         }
     }
 }
-function Show-Event {
-
-
-    [xml]$event = $EventWindowXaml
-    $EventWindowReader = (New-Object System.Xml.XmlNodeReader $event)
-    $itt.event = [Windows.Markup.XamlReader]::Load($EventWindowReader)
-    $itt.event.Resources.MergedDictionaries.Add($itt["window"].FindResource($itt.CurretTheme))
-
-
-    $itt.event.FindName('date').Text = $itt.date
-    $CloseBtn = $itt.event.FindName('closebtn')
-
-            $itt.event.FindName('title').text = 'New Update!
-'.Trim()
-
-    
-        $itt.event.FindName('contribute').add_MouseLeftButtonDown({
-                Start-Process('https://github.com/emadadel4/itt?tab=readme-ov-file#-how-to-contribute')
-            })
-        
-        $itt.event.FindName('shell').add_MouseLeftButtonDown({
-                Start-Process('https://github.com/emadadel4/shelltube')
-            })
-        
-        $itt.event.FindName('ytv').add_MouseLeftButtonDown({
-                Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')
-            })
-        
-
-    $CloseBtn.add_MouseLeftButtonDown({
-        $itt.event.Close()
-    })
-
-    $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({
-        DisablePopup
-        $itt.event.Close()
-    })
-
-    # Show dialog
-    if($itt.PopupWindow -eq "off") {return}   
-    $itt.event.ShowDialog() | Out-Null
-}
-
-function DisablePopup {
-    Set-ItemProperty -Path $itt.registryPath  -Name "PopupWindow" -Value "off" -Force
-}
 function UpdateUI {
 
     <#
@@ -12736,7 +12690,6 @@ function UpdateUI {
         $itt.$TextIcon.Text = "$icon"
     })
 }
-
 #===========================================================================
 #endregion End Main Functions
 #===========================================================================
@@ -16278,6 +16231,54 @@ $AboutWindowXaml = '<Window
 #region Begin WPF Event Window
 #===========================================================================
 
+function Show-Event {
+
+
+    [xml]$event = $EventWindowXaml
+    $EventWindowReader = (New-Object System.Xml.XmlNodeReader $event)
+    $itt.event = [Windows.Markup.XamlReader]::Load($EventWindowReader)
+    $itt.event.Resources.MergedDictionaries.Add($itt["window"].FindResource($itt.CurretTheme))
+
+
+    $itt.event.FindName('date').Text = $itt.date
+    $CloseBtn = $itt.event.FindName('closebtn')
+
+    
+        $itt.event.FindName('title').text = 'New Update!
+'.Trim()  # Set the title text
+        
+
+    
+            $itt.event.FindName('ytv').add_MouseLeftButtonDown({
+                    Start-Process('https://www.youtube.com/watch?v=QmO82OTsU5c')  # Start the process to open the URL when clicked
+                })
+            
+            $itt.event.FindName('contribute').add_MouseLeftButtonDown({
+                    Start-Process('https://github.com/emadadel4/itt?tab=readme-ov-file#-how-to-contribute')  # Start the process to open the URL when clicked
+                })
+            
+            $itt.event.FindName('shell').add_MouseLeftButtonDown({
+                    Start-Process('https://github.com/emadadel4/shelltube')  # Start the process to open the URL when clicked
+                })
+            
+
+    $CloseBtn.add_MouseLeftButtonDown({
+        $itt.event.Close()
+    })
+
+    $itt.event.FindName('DisablePopup').add_MouseLeftButtonDown({
+        DisablePopup
+        $itt.event.Close()
+    })
+
+    # Show dialog
+    if($itt.PopupWindow -eq "off") {return}   
+    $itt.event.ShowDialog() | Out-Null
+}
+
+function DisablePopup {
+    Set-ItemProperty -Path $itt.registryPath  -Name "PopupWindow" -Value "off" -Force
+}
 $EventWindowXaml = '<Window
     xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
     xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
@@ -16919,4 +16920,3 @@ Stop-Transcript
 #===========================================================================
 #endregion End Main
 #===========================================================================
-
